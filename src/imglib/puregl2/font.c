36c7bd2bc2cf39c8ca49396258ad42b3d55554fb
Jim <Jim@localhost>
2013-07-25 09:24:18 -0700

    Use correct name for case sensitive filesystems

--- src/imglib/puregl2/font.c
+++ src/imglib/puregl2/font.c
@@ -11,8 +11,8 @@
 #include <string.h>
 
 #ifdef USE_GLES2
-//#include <gles/gl.h>
-#include <gles2/gl2.h>
+//#include <GLES/gl.h>
+#include <GLES2/gl2.h>
 #else
 #include <GL/glew.h>
 #include <GL/gl.h>         // Header File For The OpenGL32 Library
