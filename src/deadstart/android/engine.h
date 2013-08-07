cb0dd573b35815dbbe52a0b140ac3f8dbbb48840
Jim <d3x0r@users.noreply.github.com>
2013-08-07 12:06:30 -0700

    Project build updates; sack sources, intershell install, ball_mixer nearly works; intershell links; and might run if packaged

--- src/deadstart/android/engine.h
+++ src/deadstart/android/engine.h
@@ -3,9 +3,9 @@
 
 #include <android/sensor.h>
 #include <android/log.h>
-#include <android_native_app_glue.h>
 #include <android/asset_manager.h>
 #include <android/native_activity.h>
+#include "android_native_app_glue.h"
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
