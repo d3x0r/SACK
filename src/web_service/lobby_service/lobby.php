52f35eed4223db4b969fd3d5c545f02450606f2e
Jim <d3x0r@users.noreply.github.com>
2013-08-26 14:41:35 -0700

    Lobby service should be pretty simple actually

---
+++ src/web_service/lobby_service/lobby.php
@@ -0,0 +1,26 @@
+
+<?php 
+
+function get_lobbies( $platform )
+{
+    $sql = "select lobby_id from games_platform_lobbies join games_platforms using(platform_id) where platform_name=$platform";
+
+    // Note the use of trigger_error instead of or die. 
+    $query = mysql_query($sql) or trigger_error("Query Failed: " . mysql_error()); 
+
+    while ($row = mysql_fetch_assoc($query)) {
+        echo $row['lobby_id'];
+    }
+
+    // Free the resources associated with the result set
+    // This is done automatically at the end of the script
+    mysql_free_result($query);
+}
+
+function set_platform( $platform )
+{
+
+}
+
+
+?>
