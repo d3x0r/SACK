ae3907761b73d433831c14e3120d7121db64ea1a
Jim <Jim@localhost>
2013-08-27 14:38:39 -0700

    Implement some staticness.  Begin implementing client websocket a little

--- src/netlib/html5.websocket/client/html5.websocket.client.c
+++ src/netlib/html5.websocket/client/html5.websocket.client.c
@@ -5,6 +5,24 @@
 
 #include "local.h"
 
+static void SendClose( WebSocketClient websock )
+{
+
+}
+
+static void CPROC WebSocketTimer( PTRSZVAL psv )
+{
+	INDEX idx;
+   WebSocketClient websock;
+	LIST_FORALL( l.clients, idx, WebSocketClient, websock )
+	{
+		if( websock->flags.want_close )
+		{
+         SendClose( websock );
+		}
+	}
+}
+
 
 // create a websocket connection.
 //  If web_socket_opened is passed as NULL, this function will wait until the negotiation has passed.
