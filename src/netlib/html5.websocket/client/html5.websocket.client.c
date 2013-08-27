318a4ee021b6a59e4decc4dc43dacb7dd6c4ce10
jim <d3x0r@users.noreply.github.com>
2013-08-27 09:32:50 -0700

    Added ball_mixer3 to get it to work with VAOs.  Adding html5 websocket client side.

---
+++ src/netlib/html5.websocket/client/html5.websocket.client.c
@@ -0,0 +1,38 @@
+#include <stdhdrs.h>
+
+#include <html5.websocket.client.h>
+
+
+#include "local.h"
+
+
+// create a websocket connection.
+//  If web_socket_opened is passed as NULL, this function will wait until the negotiation has passed.
+//  since these packets are collected at a lower layer, buffers passed to receive event are allocated for
+//  the application, and the application does not need to setup an  initial read.
+WebSocketClient WebSocketOpen( CTEXTSTR address
+															 , int options
+															 , web_socket_opened
+									  , web_socket_event, web_socket_closed, web_socket_error, psv )
+{
+   WebSocketClient websock = New( struct web_socket_client );
+   return  websock;
+}
+
+// end a websocket connection nicely.
+void WebSocketClose( WebSocketClient websock )
+{
+
+}
+
+// there is a control bit for whether the content is text or binary or a continuation
+void WebSocketSendText( WebSocketClient websock, POINTER buffer, size_t length ) // UTF8 RFC3629
+{
+
+}
+
+// literal binary sending; this may happen to be base64 encoded too
+void WebSocketSendBinary( WebSocketClient websock, POINTER buffer, size_t length )
+{
+}
+
