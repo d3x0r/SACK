ab76830e7bc56174236649ed0968762dbab72fe9
Jim <d3x0r@users.noreply.github.com>
2013-08-30 16:09:46 -0700

    tweaks for linux build(?) and apache module

--- src/utils/apache_shell/apache_shell.c
+++ src/utils/apache_shell/apache_shell.c
@@ -16,6 +16,7 @@ struct apache_interface_cdecl
 	void (CPROC *ap_set_content_type)(request_rec *r, const char *ct);
 	int  (CPROC *ap_rprintf)(request_rec *r, const char *fmt,...) __attribute__((format(printf,2,3)));
 	int  (CPROC *ap_hook_handler)(ap_HOOK_handler_t, const char * const *aszPre, const char * const *aszSucc, int nOrder);
+	int  (CPROC *ap_hook_post_read_request)(ap_HOOK_handler_t, const char * const *aszPre, const char * const *aszSucc, int nOrder);
 	void *ap_set_string_slot;
 };
 
@@ -25,6 +26,7 @@ struct apache_interface_stdcall
 	void (WINAPI *ap_set_content_type)(request_rec *r, const char *ct);
 	int  (WINAPI *ap_rprintf)(request_rec *r, const char *fmt,...) __attribute__((format(printf,2,3)));
 	int  (WINAPI *ap_hook_handler)(ap_HOOK_handler_t, const char * const *aszPre, const char * const *aszSucc, int nOrder);
+	int  (WINAPI *ap_hook_post_read_request)(ap_HOOK_handler_t, const char * const *aszPre, const char * const *aszSucc, int nOrder);
 	void *ap_set_string_slot;
 };
 
@@ -47,22 +49,25 @@ static struct {
 #define ALIAS(n,...)  ( (l.a_c_interface)?(l.a_c_interface)->n(__VA_ARGS__):((l.a_stdcall_interface)?(l.a_stdcall_interface)->n(__VA_ARGS__):0))
 #define ALIAS_F(n)  ( (l.a_c_interface)?(l.a_c_interface)->n:((l.a_stdcall_interface)?(l.a_stdcall_interface)->n:0))
 
-#define ap_set_content_type(...)    ALIAS(ap_set_content_type,__VA_ARGS__)
-#define ap_rprintf(...)             ALIAS(ap_rprintf,__VA_ARGS__)
-#define ap_hook_handler(...)        ALIAS(ap_hook_handler,__VA_ARGS__)
+#define ap_set_content_type(...)        ALIAS(ap_set_content_type,__VA_ARGS__)
+#define ap_rprintf(...)                 ALIAS(ap_rprintf,__VA_ARGS__)
+#define ap_hook_handler(...)            ALIAS(ap_hook_handler,__VA_ARGS__)
+#define ap_hook_post_read_request(...)  ALIAS(ap_hook_post_read_request,__VA_ARGS__)
 //#define ap_set_string_slot(...)     ALIAS(ap_set_string_slot,__VA_ARGS__)
 
 #define ap_set_string_slot     ALIAS(ap_set_string_slot)
 
 PRELOAD( InitApacheModule )
 {
-   l.__a_c_interface.ap_set_content_type = LoadFunction( l.server_core, "ap_set_content_type" );
-   l.__a_c_interface.ap_rprintf          = LoadFunction( l.server_core, "ap_rprintf" );
-   l.__a_c_interface.ap_hook_handler     = LoadFunction( l.server_core, "ap_hook_handler" );
+	l.__a_c_interface.ap_set_content_type       = LoadFunction( l.server_core, "ap_set_content_type" );
+	l.__a_c_interface.ap_rprintf                = LoadFunction( l.server_core, "ap_rprintf" );
+	l.__a_c_interface.ap_hook_handler           = LoadFunction( l.server_core, "ap_hook_handler" );
+	l.__a_c_interface.ap_hook_post_read_request = LoadFunction( l.server_core, "ap_hook_post_read_request" );
 
-   l.__a_stdcall_interface.ap_set_content_type = LoadFunction( l.server_core, "ap_set_content_type" );
-   l.__a_stdcall_interface.ap_rprintf          = LoadFunction( l.server_core, "ap_rprintf" );
-	l.__a_stdcall_interface.ap_hook_handler     = LoadFunction( l.server_core, "ap_hook_handler" );
+	l.__a_stdcall_interface.ap_set_content_type        = LoadFunction( l.server_core, "ap_set_content_type" );
+	l.__a_stdcall_interface.ap_rprintf                 = LoadFunction( l.server_core, "ap_rprintf" );
+	l.__a_stdcall_interface.ap_hook_handler            = LoadFunction( l.server_core, "ap_hook_handler" );
+	l.__a_stdcall_interface.ap_hook_post_read_request  = LoadFunction( l.server_core, "ap_hook_post_read_request" );
 
    // setup one of the interfaces... 50-50 chance to get it right?
    //l.a_c_interface = &l.__a_c_interface;
@@ -106,10 +111,10 @@ static int example_handler(request_rec *r)
     return OK;
 }
 
-static void* register_dir_handler( apr_pool_t *pool, char *x )
+static void*  register_dir_handler( apr_pool_t *pool, char *x )
 {
 	struct dir_data *dir = New( struct dir_data );
-	lprintf( "Adding to %s", x );
+	lprintf( "Adding to %s", x?x:"<global>" );
 	dir->name = x;
 	return dir;
 }
@@ -128,12 +133,21 @@ static void* register_dir_handler( apr_pool_t *pool, char *x )
 }	*/
 ;
 
+static void first_post( apr_pool_t *pool )
+{
+   lprintf( "here" );
+
+}
+
 static void register_hooks(apr_pool_t *pool)
 {
 	/* Create a hook in the request handler, so we get called when a request arrives */
    InvokeDeadstart();
    lprintf( "here" );
-    ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_LAST);
+	ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_FIRST);
+   ap_hook_post_read_request( first_post, NULL, NULL, APR_HOOK_FIRST );
+									  // ap_hook_fixups() ; // last chance hook before content generation
+                             // ap_hook_log_transaction
 }
 
 module AP_MODULE_DECLARE_DATA org_d3x0r_sack_apache_module =
