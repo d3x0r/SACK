a110f3ccb7795b470f58e90fe82800478a1f4d4c
Jim <Jim@localhost>
2013-09-11 11:55:57 -0700

    Connect html5 test projects to make system

--- src/netlib/html5.websocket/server/test.html5.caller.c
+++ src/netlib/html5.websocket/server/test.html5.caller.c
@@ -7,7 +7,7 @@
 
 int main( void )
 {
-	HTML5WebSocket socket = CreateWebSocket( "0.0.0.0:9998" );
+	PCLIENT socket = WebSocketCreate( "0.0.0.0:9998" );
 
 	while( 1 )
       WakeableSleep( 10000 );
