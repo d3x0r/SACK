e63bd7908a417697724db7a2c6452394de6c324b
Jim <d3x0r@users.noreply.github.com>
2013-09-14 04:09:48 -0700

    update header

--- include/json_emitter.h
+++ include/json_emitter.h
@@ -36,7 +36,7 @@ struct json_context_object;
 struct json_context;
 
 
-#define JSON_NO_OFFSET -1
+#define JSON_NO_OFFSET (size_t)-1
 
 // Get a context, which can track message formats.
 // Will eventually expose the low level routines so one can use a context
@@ -49,7 +49,11 @@ JSON_EMITTER_PROC( struct json_context *, json_create_context )( void );
 JSON_EMITTER_PROC( struct json_context_object *, json_create_object )( struct json_context *context );
 // Begin the definition of a json formatting object.
 // the root element must be a array or an object
-JSON_EMITTER_PROC( struct json_context_object *, json_create_array )( struct json_context *context, int offset, int count, int count_offset );
+JSON_EMITTER_PROC( struct json_context_object *, json_create_array )( struct json_context *context
+																	 , size_t offset
+																	 , int type
+																	 , size_t count
+																	 , size_t count_offset );
 
 // add a member element to a json object
 // if the member element is a object type, then a new context_object results, to which members may be added.
