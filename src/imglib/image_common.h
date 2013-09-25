9198b600067ba7b54332b9f3a7a656ac3383b855
Jim <d3x0r@users.noreply.github.com>
2013-09-25 16:12:22 -0700

    better merged code for fonts; still no output d3d.  Stripped sources

--- src/imglib/image_common.h
+++ src/imglib/image_common.h
@@ -1,4 +1,8 @@
 
+ASM_IMAGE_NAMESPACE
+_32 DOALPHA( _32 over, _32 in, _8 a );
+ASM_IMAGE_NAMESPACE_END
+
 IMAGE_NAMESPACE
 
 void  CPROC cSetColorAlpha( PCDATA po, int oo, int w, int h, CDATA color );
