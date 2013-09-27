475b4b8bc697cf7157a0e8363279b45888969949
Jim <d3x0r@users.noreply.github.com>
2013-09-26 23:48:58 -0700

    Add protection to try and not deadlock on exit; wasn't the issue but should be done

--- src/vidlib/puregl2/vidlib_win32.c
+++ src/vidlib/puregl2/vidlib_win32.c
@@ -40,10 +40,16 @@ extern KEYDEFINE KeyDefs[];
 ATEXIT( ExitTest )
 {
 	l.bExitThread = 1;
-   // just to make sure something wakes it up... it could be in a mouse event.
-	PostThreadMessage (l.dwThreadID, WM_USER_SHUTDOWN, 0, 0);
-	while(l.bThreadRunning)
-      Relinquish();
+	if( l.dwThreadID != GetCurrentThreadId () )
+	{
+		// just to make sure something wakes it up... it could be in a mouse event.
+		PostThreadMessage (l.dwThreadID, WM_USER_SHUTDOWN, 0, 0);
+		while(l.bThreadRunning)
+		{
+         lprintf( "waiting..." );
+			Relinquish();
+		}
+	}
 }
 
 
