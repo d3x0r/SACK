58377f6a010e3ebf0d1c06490d56fcf52d78deb7
d3x0r <d3x0r@localhost>
2013-11-30 09:33:37 -0800

    TInkering with some 3d stuff

---
+++ src/games/seg/seg.cpp
@@ -0,0 +1,52 @@
+
+// Source mostly from
+
+#define MAKE_RCOORD_SINGLE
+#define NO_FILEOP_ALIAS
+#include <stdhdrs.h>
+#define USE_RENDER_INTERFACE l.pri
+#define USE_IMAGE_INTERFACE l.pii
+#define NEED_VECTLIB_COMPARE
+
+// define local instance.
+#define TERRAIN_MAIN_SOURCE  
+#include <vectlib.h>
+#include <render.h>
+#include <render3d.h>
+
+#ifndef SOMETHING_GOES_HERE
+#include <GL/glew.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include <virtuality.h>
+//#include "local.h"
+
+
+
+
+static PTRSZVAL OnInit3d( "Seg Renderer" )( PMatrix projection, PTRANSFORM camera, RCOORD *identity_depth, RCOORD *aspect )
+{
+	MakeGlider( );
+
+	return 1;
+}
+
+static void OnFirstDraw3d( "Seg Renderer" )( PTRSZVAL psvInit )
+{
+}
+
+static void OnBeginDraw3d( "Seg Renderer" )( PTRSZVAL psvInit, PTRANSFORM camera )
+{
+
+}
+
+static void OnDraw3d( "Seg Renderer" )( PTRSZVAL psvInit )
+{
+}
+
+static LOGICAL OnMouse3d( "Virtuality" )( PTRSZVAL psvInit, PRAY mouse, _32 b )
+{
+	return 0;
+}
