b2ab7ed55b8037787ee3a194c28cdc1636c039f1
jim <jim@localhost>
2014-01-12 05:17:10 -0800

    Begin implementation of a method to draw a open plane... a plane that has failed full clipping

--- src/games/virtuality/global.h
+++ src/games/virtuality/global.h
@@ -1,6 +1,7 @@
 
 #ifndef FORCE_NO_INTERFACE
 #define USE_RENDER_INTERFACE g.pri
+#define USE_RENDER_3D_INTERFACE g.pr3i
 #define USE_IMAGE_INTERFACE g.pii
 #endif
 
@@ -15,12 +16,18 @@
 #endif
 #include <timers.h>
 
+#include <virtuality.h>
+
 #ifndef VIEW_MAIN
 extern
 #endif
 struct global_virtuality_data_tag {
 	PIMAGE_INTERFACE pii;
-   PRENDER_INTERFACE pri;
+	PRENDER_INTERFACE pri;
+	PRENDER3D_INTERFACE pr3i;
+
+	PMYLINESEGSET *ppLinePool; // common pool for utilities
+
 } global_virtuality_data;
 
 #define g global_virtuality_data
