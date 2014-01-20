29c433b0ee1570c9107618456eec707bd0bf658b
d3x0r <d3x0r@users.noreply.github.com>
2014-01-20 02:02:54 -0800

    Add option to load frame without creating an editable one; just return NULL; Have converter program use new option and not create an editable frame.

--- src/utils/frame_ID_converter/convert.c
+++ src/utils/frame_ID_converter/convert.c
@@ -7,7 +7,7 @@ SaneWinMain( argc, argv )
 {
 	for( ; argc > 1; argc--, argv++ )
 	{
-		PSI_CONTROL frame = LoadXMLFrame( argv[1] );
+		PSI_CONTROL frame = LoadXMLFrameOverOption( NULL, argv[1], 0 );
 		if( frame )
 			SaveXMLFrame( frame, argv[1] );
 	}
