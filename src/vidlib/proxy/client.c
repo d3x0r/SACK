63ba46d24d7eb056b2ade0872afa0cc48b83dea7
jim <d3x0r@users.noreply.github.com>
2014-02-03 11:07:48 -0800

    Keep client render structure with original render reference for later operations; otherwise the surface may change

--- src/vidlib/proxy/client.c
+++ src/vidlib/proxy/client.c
@@ -1,9 +1,9 @@
 #include <stdhdrs.h>
 
-#define USE_RENDER_INTERFACE proxy_client_local.pri
-#define USE_IMAGE_INTERFACE proxy_client_local.pii
-#define USE_RENDER3D_INTERFACE proxy_client_local.pr3i
-#define USE_IMAGE_3D_INTERFACE proxy_client_local.pi3i
+#define USE_RENDER_INTERFACE l.pri
+#define USE_IMAGE_INTERFACE l.pii
+#define USE_RENDER3D_INTERFACE l.pr3i
+#define USE_IMAGE_3D_INTERFACE l.pi3i
 #include <render.h>
 #include <render3d.h>
 
@@ -53,20 +53,20 @@ static void CPROC SocketRead( PCLIENT pc, POINTER buffer, size_t size )
 			case PMID_SetApplicationIcon:  // 2 (unused)
 				break;
 			case PMID_OpenDisplayAboveUnderSizedAt:  // 3
-				SetLink( &proxy_client_local.renderers, msg->data.opendisplay_data.server_display_id
+				SetLink( &l.renderers, msg->data.opendisplay_data.server_display_id
 				    , r = OpenDisplayAboveUnderSizedAt( msg->data.opendisplay_data.attr
 				                                    , msg->data.opendisplay_data.w
 				                                    , msg->data.opendisplay_data.h
 				                                    , msg->data.opendisplay_data.x
 				                                    , msg->data.opendisplay_data.y
-				                                    , (PRENDERER)GetLink( &proxy_client_local.renderers, msg->data.opendisplay_data.over )
-				                                    , (PRENDERER)GetLink( &proxy_client_local.renderers, msg->data.opendisplay_data.under )
+				                                    , (PRENDERER)GetLink( &l.renderers, msg->data.opendisplay_data.over )
+				                                    , (PRENDERER)GetLink( &l.renderers, msg->data.opendisplay_data.under )
 				                                  ) ); 
 				RestoreDisplay( r );
 				break;
 			case PMID_CloseDisplay:  // 4
 				{
-					PRENDERER r = (PRENDERER)GetLink( &proxy_client_local.renderers, msg->data.close_display.server_display_id );
+					PRENDERER r = (PRENDERER)GetLink( &l.renderers, msg->data.close_display.server_display_id );
 					if( r )
 						CloseDisplay( r );
 				}
@@ -75,103 +75,135 @@ static void CPROC SocketRead( PCLIENT pc, POINTER buffer, size_t size )
 				{
 					if( msg->data.make_image.server_display_id != INVALID_INDEX )
 					{
-						SetLink( &proxy_client_local.images, msg->data.make_image.server_image_id
-						        , GetDisplayImage( (PRENDERER)GetLink( &proxy_client_local.renderers, msg->data.make_image.server_display_id ) )
+						struct client_proxy_image *image = New( struct client_proxy_image );
+                  image->image = NULL;
+                  image->render_id = msg->data.make_image.server_display_id;
+						SetLink( &l.images, msg->data.make_image.server_image_id
+						        , image
 						       );
 					}
 					else
-						SetLink( &proxy_client_local.images, msg->data.make_image.server_image_id
-						    , MakeImageFile( msg->data.make_image.w, msg->data.make_image.h )
-						    );
+					{
+						struct client_proxy_image *image = New( struct client_proxy_image );
+                  image->image = MakeImageFile( msg->data.make_image.w, msg->data.make_image.h );
+                  image->render_id = INVALID_INDEX;
+						SetLink( &l.images, msg->data.make_image.server_image_id
+								 , image
+								 );
+					}
 					break;
 				}
 				break;
 			case PMID_MakeSubImage: // 7
-				lprintf( "make sub on %p", GetLink( &proxy_client_local.images, msg->data.make_subimage.server_parent_image_id ) );
-				SetLink( &proxy_client_local.images, msg->data.make_subimage.server_image_id
-				    , MakeSubImage( (Image)GetLink( &proxy_client_local.images, msg->data.make_subimage.server_parent_image_id )
-				                    , msg->data.make_subimage.x
-				                    , msg->data.make_subimage.y
-				                    , msg->data.make_subimage.w
-				                    , msg->data.make_subimage.h
-				                    ) 
-				        );
-				UpdateDisplay( (PRENDERER)GetLink( &proxy_client_local.renderers, 0 ) );
+				{
+					struct client_proxy_image *image = New( struct client_proxy_image );
+					struct client_proxy_image *parent_image = GetLink( &l.images, msg->data.make_subimage.server_parent_image_id );
+					image->image = MakeSubImage( parent_image->image
+														, msg->data.make_subimage.x
+														, msg->data.make_subimage.y
+														, msg->data.make_subimage.w
+														, msg->data.make_subimage.h
+														);
+					image->render_id = INVALID_INDEX;
+				}
 				break;
 			case PMID_BlatColor: // 8
-				BlatColor( (Image)GetLink( &proxy_client_local.images, msg->data.blatcolor.server_image_id )
-				        , msg->data.blatcolor.x
-				        , msg->data.blatcolor.y
-				        , msg->data.blatcolor.w
-				        , msg->data.blatcolor.h 
-				        , msg->data.blatcolor.color
-				        );
+				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blatcolor.server_image_id );
+					Image real_image = image->image?image->image:GetDisplayImage( GetLink( &l.renderers, image->render_id ) );
+					BlatColor( real_image
+								, msg->data.blatcolor.x
+								, msg->data.blatcolor.y
+								, msg->data.blatcolor.w
+								, msg->data.blatcolor.h
+								, msg->data.blatcolor.color
+								);
+				}
 				break;
 			case PMID_BlatColorAlpha: // 9 
-				BlatColorAlpha( (Image)GetLink( &proxy_client_local.images, msg->data.blatcolor.server_image_id )
-				        , msg->data.blatcolor.x
-				        , msg->data.blatcolor.y
-				        , msg->data.blatcolor.w
-				        , msg->data.blatcolor.h 
-				        , msg->data.blatcolor.color
-				        );
+				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blatcolor.server_image_id );
+					Image real_image = image->image?image->image:GetDisplayImage( GetLink( &l.renderers, image->render_id ) );
+					BlatColorAlpha( (Image)GetLink( &l.images, msg->data.blatcolor.server_image_id )
+									  , msg->data.blatcolor.x
+									  , msg->data.blatcolor.y
+									  , msg->data.blatcolor.w
+									  , msg->data.blatcolor.h
+									  , msg->data.blatcolor.color
+									  );
+				}
 				break;
 			case PMID_ImageData: // 10 - transfer local image data to client
 				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blatcolor.server_image_id );
 					Image tmp;
-					tmp = GetLink( &proxy_client_local.images, msg->data.image_data.server_image_id );
-					if( tmp )
-						UnmakeImageFile( tmp );
+					if( image->image )
+						UnmakeImageFile( image->image );
 					// reloadimage sorta
 					// could include the math; the message header is 1 byte (subtract from size)
 					// the image-data_data contains one byte, and that needs to (not subtract from size) 
 					// so thats +0 to sizeof image_data_data
-					SetLink( &proxy_client_local.images, msg->data.image_data.server_image_id
-							, DecodeMemoryToImage( msg->data.image_data.data, size - ( sizeof( struct image_data_data ) ) )
-						   );
+					image->image = DecodeMemoryToImage( msg->data.image_data.data, size - ( sizeof( struct image_data_data ) ) );
 				}
 				break;
 			case PMID_BlotImageSizedTo:  // 11
-				lprintf( "Ouptut 5o %p",  GetLink( &proxy_client_local.images, msg->data.blot_image.server_image_id ) );
-            lprintf( "Might output to %p", GetDisplayImage( GetLink( &proxy_client_local.renderers, 0 ) ) );
-				BlotImageSizedEx( (Image)GetLink( &proxy_client_local.images, msg->data.blot_image.server_image_id )
-					, (Image)GetLink( &proxy_client_local.images, msg->data.blot_image.image_id )
-				        , msg->data.blot_image.x
-				        , msg->data.blot_image.y
-						, msg->data.blot_image.xs
-				        , msg->data.blot_image.ys
-				        , msg->data.blot_image.w
-				        , msg->data.blot_image.h 
-						, TRUE
-						, BLOT_COPY
-				        );
+				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blot_image.server_image_id );
+					Image real_image = image->image?image->image:GetDisplayImage( GetLink( &l.renderers, image->render_id ) );
+					lprintf( "Ouptut 5o %p",  GetLink( &l.images, msg->data.blot_image.server_image_id ) );
+					lprintf( "Might output to %p", GetDisplayImage( GetLink( &l.renderers, 0 ) ) );
+					BlotImageSizedEx( (Image)GetLink( &l.images, msg->data.blot_image.server_image_id )
+										 , real_image
+										 , msg->data.blot_image.x
+										 , msg->data.blot_image.y
+										 , msg->data.blot_image.xs
+										 , msg->data.blot_image.ys
+										 , msg->data.blot_image.w
+										 , msg->data.blot_image.h
+										 , TRUE
+										 , BLOT_COPY
+										 );
+				}
 				break;
 			case PMID_BlotScaledImageSizedTo: // 12
-				BlotScaledImageSizedEx( (Image)GetLink( &proxy_client_local.images, msg->data.blot_scaled_image.server_image_id )
-					, (Image)GetLink( &proxy_client_local.images, msg->data.blot_scaled_image.image_id )
-				        , msg->data.blot_scaled_image.x
-				        , msg->data.blot_scaled_image.y
-				        , msg->data.blot_scaled_image.w
-				        , msg->data.blot_scaled_image.h 
-						, msg->data.blot_scaled_image.xs
-				        , msg->data.blot_scaled_image.ys
-				        , msg->data.blot_scaled_image.ws
-				        , msg->data.blot_scaled_image.hs 
-						, TRUE
-						, BLOT_COPY
-				        );
+				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blot_scaled_image.server_image_id );
+					Image real_image = image->image?image->image:GetDisplayImage( GetLink( &l.renderers, image->render_id ) );
+					BlotScaledImageSizedEx( (Image)GetLink( &l.images, msg->data.blot_scaled_image.server_image_id )
+												 , real_image
+												 , msg->data.blot_scaled_image.x
+												 , msg->data.blot_scaled_image.y
+												 , msg->data.blot_scaled_image.w
+												 , msg->data.blot_scaled_image.h
+												 , msg->data.blot_scaled_image.xs
+												 , msg->data.blot_scaled_image.ys
+												 , msg->data.blot_scaled_image.ws
+												 , msg->data.blot_scaled_image.hs
+												 , TRUE
+												 , BLOT_COPY
+												 );
+				}
 				break;
 			case PMID_DrawLine: // 13
-				do_lineAlpha( (Image)GetLink( &proxy_client_local.images, msg->data.line.server_image_id )
-					, msg->data.line.x1
-					, msg->data.line.y1
-					, msg->data.line.x2
-					, msg->data.line.y2
-					, msg->data.line.color );
+				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blatcolor.server_image_id );
+					Image real_image = image->image?image->image:GetDisplayImage( GetLink( &l.renderers, image->render_id ) );
+					do_lineAlpha( real_image
+									, msg->data.line.x1
+									, msg->data.line.y1
+									, msg->data.line.x2
+									, msg->data.line.y2
+									, msg->data.line.color );
+				}
 				break;
 			case PMID_UnmakeImage: // 14
-				UnmakeImageFile( (Image)GetLink( &proxy_client_local.images, msg->data.unmake_image.server_image_id ) );
-				SetLink( &proxy_client_local.images, msg->data.unmake_image.server_image_id, 0 );
+				{
+					struct client_proxy_image *image = GetLink( &l.images, msg->data.blatcolor.server_image_id );
+					Image real_image = image->image?image->image:GetDisplayImage( GetLink( &l.renderers, image->render_id ) );
+					UnmakeImageFile( real_image );
+					SetLink( &l.images, msg->data.unmake_image.server_image_id, 0 );
+					Release( image );
+				}
 				break;
 			//case PMID_Event_Mouse // 15 (from client to server)
 			//case PMID_Event_Key // 16 (from client to server)
@@ -192,22 +224,22 @@ static void CPROC SocketRead( PCLIENT pc, POINTER buffer, size_t size )
 
 static void CPROC SocketClose( PCLIENT pc )
 {
-	proxy_client_local.service = NULL;
-	WakeThread( proxy_client_local.main );
+	l.service = NULL;
+	WakeThread( l.main );
 }
 
 SaneWinMain( argc, argv )
 {
 	NetworkStart();
-	proxy_client_local.service = OpenTCPClientExx( argv[1]?argv[1]:WIDE("127.0.0.1"), 4241, SocketRead, SocketClose, NULL, NULL );
-	while( proxy_client_local.service )
+	l.service = OpenTCPClientExx( argv[1]?argv[1]:WIDE("127.0.0.1"), 4241, SocketRead, SocketClose, NULL, NULL );
+	while( l.service )
 	{
-		proxy_client_local.pri = GetDisplayInterface();
-		proxy_client_local.pii = GetImageInterface();
-		proxy_client_local.pr3i = GetRender3dInterface();
-		proxy_client_local.pi3i = GetImage3dInterface();
+		l.pri = GetDisplayInterface();
+		l.pii = GetImageInterface();
+		l.pr3i = GetRender3dInterface();
+		l.pi3i = GetImage3dInterface();
 
-		proxy_client_local.main = MakeThread();
+		l.main = MakeThread();
 		WakeableSleep( 10000 );
 	}
 	return 0;
