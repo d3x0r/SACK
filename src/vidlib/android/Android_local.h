53867a74b5aa165432bc4a6748203ea64c6e130a
d3x0r <d3x0r@localhost>
2014-02-15 20:04:35 -0800

    Fix android native window output (?)

--- src/vidlib/android/Android_local.h
+++ src/vidlib/android/Android_local.h
@@ -1,38 +1,4 @@
 
-#include <network.h>
-#include "protocol.h"
-
-typedef struct vidlib_proxy_image
-{
-	int x, y, w, h;
-	int string_mode;
-	int blot_method;
-	struct vidlib_proxy_image *parent;
-	struct vidlib_proxy_image *child;
-	struct vidlib_proxy_image *next;
-	struct vidlib_proxy_image *prior;
-
-	INDEX filegroup;
-	TEXTSTR filename;
-	Image image;
-	INDEX render_id;
-	INDEX id;
-
-	struct vidlib_proxy_image_flags {
-		BIT_FIELD bUsed : 1;
-		BIT_FIELD bLocked : 1;
-	} flags;
-
-} *PVPImage;
-
-struct server_socket_state
-{
-	struct server_socket_flags {
-		BIT_FIELD get_length : 1;
-	} flags;
-	POINTER buffer;
-	int read_length;
-};
 
 typedef struct vidlib_proxy_renderer
 {
@@ -40,8 +6,7 @@ typedef struct vidlib_proxy_renderer
 	S_32 x, y;
 	_32 attributes;
 	struct vidlib_proxy_renderer *above, *under;
-	PLIST remote_render_id;  // this is synced with same index as l.clients
-	struct vidlib_proxy_image *image;  // representation of the output surface
+	Image image;  // representation of the output surface
 	struct vidlib_proxy_renderer_flags
 	{
 		BIT_FIELD hidden : 1;
@@ -64,6 +29,9 @@ struct vidlib_proxy_local
 	PIMAGE_INTERFACE real_interface;
 	_8 key_states[256];
 	CRITICALSECTION message_formatter;
+	ANativeWindow *displayWindow;
+   _32 default_display_x, default_display_y;
+
 } l;
 
 CTEXTSTR SACK_Vidlib_GetKeyText( int pressed, int key_index, int *used );
