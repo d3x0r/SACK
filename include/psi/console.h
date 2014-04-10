0841e45579136abd639cfe88b70f06591db884da
jim <d3x0r@users.noreply.github.com>
2014-04-10 15:00:55 -0700

    begin extending PSI console interface

--- include/psi/console.h
+++ include/psi/console.h
@@ -26,14 +26,38 @@
 
 PSI_NAMESPACE
 	_CONSOLE_NAMESPACE
+
+
+typedef struct PSI_phase *PSI_Phrase;
+typedef struct PSI_feedback *PSI_Feedback;
+typedef void (*PSI_FeedbackClick)( PTRSZVAL psv_user, PSI_Feedback, PTEXT line );
+typedef struct PSI_draw_context *PSI_DrawContext;
+
 PSI_CONSOLE_PROC( void, PSIConsoleLoadFile )( PSI_CONTROL pc, CTEXTSTR filename );
 PSI_CONSOLE_PROC( void, PSIConsoleSaveFile )( PSI_CONTROL pc, CTEXTSTR filename );
 
 PSI_CONSOLE_PROC( int, vpcprintf )( PSI_CONTROL pc, CTEXTSTR format, va_list args );
 PSI_CONSOLE_PROC( int, pcprintf )( PSI_CONTROL pc, CTEXTSTR format, ... );
 PSI_CONSOLE_PROC( int, PSIConsoleOutput )( PSI_CONTROL pc, PTEXT lines );
+
+// this results with a context structure that allows referencing the line in the console in the future..
+PSI_CONSOLE_PROC( PSI_Phrase, PSIConsolePhraseOutput )( PSI_CONTROL pc, PTEXT lines );
+
+/* measure and draw */
+
+PSI_CONSOLE_PROC( PSI_DrawContext, PSIConsoleCreateDrawContext )( PSI_CONTROL pc, PSI_ConsoleMeausre, PSI_ConsoleOutput );
+PSI_CONSOLE_PROC( PSI_Phrase, PSIConsoleOwnerDrawnOutput )( PSI_CONTROL pc, PSI_DrawContext, PTRSZVAL psv_user_data );
+
 PSI_CONSOLE_PROC( void, PSIConsoleInputEvent )( PSI_CONTROL pc, void(CPROC*Event)(PTRSZVAL,PTEXT), PTRSZVAL psv );
 PSI_CONSOLE_PROC( void, PSIConsoleSetLocalEcho )( PSI_CONTROL pc, LOGICAL yesno );
+
+
+PSI_CONSOLE_PROC( PSI_Feedback, PSI_DefineFeedback )( click, PTRSZVAL );
+
+// during a feedback event, this can be used to remove the current mesasge.
+PSI_CONSOLE_PROC( PSI_Feedback, PSI_FeedbackDelete )( PSI_Feedback context );
+
+PSI_CONSOLE_PROC( void, PSIConsoleSetMessageFeedbackHandler )( PSI_CONTROL pc, PSI_Feedback );
 
 // this is an access into to use console features to wrap long text into a block.
 PSI_CONSOLE_PROC( void, FormatTextToBlock )( CTEXTSTR input, TEXTSTR *output, int char_width, int char_height );
