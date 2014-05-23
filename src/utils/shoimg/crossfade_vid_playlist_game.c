f2d3a42d116322ffed8404a026472c82e8db0755
Jim <Jim@localhost>
2014-05-23 00:35:39 -0700

    Checkpoint update video sources

--- src/utils/shoimg/crossfade_vid_playlist_game.c
+++ src/utils/shoimg/crossfade_vid_playlist_game.c
@@ -13,13 +13,14 @@
 #include <render.h>
 #include <image.h>
 #include <timers.h>
-
+#include <mt19937ar.h>
 #include "vlcint.h"
 
 struct video_player {
 	PRENDERER surface;
 	struct my_vlc_interface *vlc;
 	_32 fade_in_time;
+	int playing;  // if stopped, don't re-stop
 };
 
 typedef struct video_element {
@@ -37,6 +38,7 @@ typedef struct video_sequence {
 	PLIST images;  // struct video_element *
 	//PLIST image_type;
 	int current_image; // overall counter, stepped appropriately.
+	int prize_count;
 };
 
 typedef struct global_tag {
@@ -65,9 +67,11 @@ typedef struct global_tag {
 	_32 target_in_start;  //
 
 	PRENDERER prior_up;
+	struct video_element *prior_video;
 	PLIST video_displays;
 
 	PLIST playlists;  // struct video_sequence *
+	int nPlayerLists;
 	int nTotalImages;
 
 	int x;
@@ -79,6 +83,15 @@ typedef struct global_tag {
 	_32 next_chain;
 	_32 want_next_chain;
 	_32 last_received;
+
+	LOGICAL attract_mode;
+	_32 number_collector;
+	_32 value_collector[64];
+	int value_collect_index;
+	int begin_card;
+
+	int prize_total;
+	struct mersenne_rng *rng;
 } GLOBAL;
 static GLOBAL g;
 
@@ -111,6 +124,7 @@ struct video_sequence *GetSequenceEx( int ID, LOGICAL bCreate )
 		seq = New( struct video_sequence );
 		MemSet( seq, 0, sizeof( struct video_sequence ) );
 		seq->sequence = ID;
+		g.nPlayerLists++;
 		AddLink( &g.playlists, seq );
 	}
 	else
@@ -121,13 +135,14 @@ struct video_sequence *GetSequenceEx( int ID, LOGICAL bCreate )
 void BeginNewChain( _32 tick )
 {
 	// if I'm in a transition, don't do this, otherwise we're not in
-   // a transition, and can now start a new transition.
+	// a transition, and can now start a new transition.
 	if( !g.target_in )
 	{
 		struct video_sequence *seq = GetSequence( g.next_chain - 1 );
 		struct video_element *element;
 		if( seq->nImages )
 		{
+			lprintf( "Begin New Chain.... %d");
 			g.current_list[g.next_up] = seq;
 			g.target_in_start = tick;
 			g.target_in = 0;  // clear this, so we start a new fade.
@@ -154,8 +169,10 @@ void BeginFadeIn( _32 tick_start, _32 tick )
 	struct video_sequence *current_seq = g.current_list[g.next_up];
 	struct video_element *current_element = (struct video_element *)GetLink( &current_seq->images, current_seq->current_image );
 	PRENDERER r;
-	//lprintf( WIDE("Begin fading in the new image...") );
-	
+	lprintf( WIDE("Begin fading in the new image... %d  %d"), current_seq->sequence, current_seq->current_image );
+	if( current_seq->sequence == 0 )
+		g.attract_mode = 1;
+
 	g.currents[g.next_up] = current_seq->current_image;
 	
 	g.target_in = g.target_in_start + (current_element->fade_in?current_element->fade_in:g.default_fade_in);
@@ -172,8 +189,8 @@ void BeginFadeIn( _32 tick_start, _32 tick )
 		PlayItem( player->vlc ); // start the video clip now... allow fadein.
 	}
 
-	//lprintf( WIDE("Begin Showing %p"), r );
-	//lprintf( WIDE("(!)Setting fade to..> %d"), 255 - (255*(now-g.target_in_start))/(g.target_in-g.target_in_start) );
+	lprintf( WIDE("Begin Showing %p"), r );
+	lprintf( WIDE("(!)Setting fade to..> %d"), 255 - (255*(tick-g.target_in_start))/(g.target_in-g.target_in_start) );
 	SetDisplayFade( r, 255 - (255*(tick-g.target_in_start))/(g.target_in-g.target_in_start) );
 	RestoreDisplay( r );
 
@@ -188,7 +205,6 @@ void ContinueFadeIn( _32 tick )
 		// completed the fade, finish, set total opaque.
 		int upd = 0;
 		PRENDERER r;
-
 		if( current_element->type == 1 )
 		{
 			upd = 1;
@@ -206,7 +222,15 @@ void ContinueFadeIn( _32 tick )
 			// wait until stop event to set when next fadin starts.
 			g.target_in_start = current_element->display_time ? (tick+current_element->display_time) : 0;
 		}
-		//lprintf( WIDE("Fade is is complete, set alpha to 0 (opaque), and hide old display...") );
+		lprintf( WIDE("Fade is is complete, set alpha to 0 (opaque), and hide old display...%d"), current_seq->sequence );
+		if( g.prior_video && ( g.prior_video->type == 2 ) )
+		{
+			if( g.prior_video->player->playing )
+			{
+				g.prior_video->player->playing = 0;
+				StopItem( g.prior_video->player->vlc );
+			}
+		}
 		SetDisplayFade( r, 0 );
 		if( upd )
 			UpdateDisplay( r );
@@ -215,26 +239,34 @@ void ContinueFadeIn( _32 tick )
 			HideDisplay( g.prior_up );
 
 		g.prior_up = r;
+		g.prior_video = current_element;
 
-		//lprintf( WIDE("Image fully up - setup to show next image...") );
 		current_seq->current_image++;
+		lprintf( WIDE("Image fully up - setup to show next image... %d,%d"), current_seq->current_image, current_seq->nImages );
 		if( current_seq->current_image >= current_seq->nImages )
 		{
+			lprintf( "out of images on this list... reset to attract" );
 			current_seq->current_image = 0;
 			if( current_seq->sequence )
 			{
-				// next up is chain 0. (plus 1 to inidicate chain change)
-				g.want_next_chain = 1;
+				lprintf( "next is attract..." );
+				// next up is chain 0. (plus 1 to inidicate chain change; 0 is no change)
+				g.want_next_chain = 0 + 1;
+				lprintf( "Setting nexup to getlink 0" );
 				g.current_list[g.next_up] = (struct video_sequence*)GetLink( &g.playlists, 0 );
 			}
 			else
+			{
+				lprintf( "next is still this... %d", current_seq->sequence );
 				g.current_list[g.next_up] = current_seq;
+			}
 
 		}
 		else
+		{
+			lprintf( "next is  still this? %d", current_seq->sequence );
 			g.current_list[g.next_up] = current_seq;
-
-
+		}
 		g.target_in = 0;
 	}
 	else
@@ -253,8 +285,8 @@ void ContinueFadeIn( _32 tick )
 			struct video_player *player = current_element->player;
 			r = player->surface;
 		}
-		//lprintf( WIDE("Setting fade to..> %d"), 255 - (255*(tick-target_in_start))/(target_in-target_in_start) );
-		//lprintf( WIDE("Increasing %p"), r );
+		lprintf( WIDE("Setting fade to..> %d"), 255 - (255*(tick-g.target_in_start))/(g.target_in-g.target_in_start) );
+		lprintf( WIDE("Increasing %p"), r );
 		SetDisplayFade( r, 255 - (255*(tick-g.target_in_start))/(g.target_in-g.target_in_start) );
 		if( upd )
 			UpdateDisplay( r );
@@ -290,7 +322,13 @@ void CPROC tick( PTRSZVAL psv )
 static void CPROC OnStopFade( PTRSZVAL psv )
 {
 	struct video_player *player = (struct video_player*)psv;
-	g.next_chain = g.want_next_chain;
+	player->playing = 0;
+	if( g.want_next_chain )
+	{
+		g.next_chain = g.want_next_chain;
+		g.want_next_chain = 0;
+	}
+	lprintf( "Video ended; begin chain: %d", g.next_chain );
 	g.target_in_start = GetTickCount(); // tick now.
 	StopItem( player->vlc );
 }
@@ -309,7 +347,7 @@ static struct video_element *LoadVideo( struct video_sequence *seq, CTEXTSTR fil
 	player->vlc = PlayItemOnEx( player->surface, file, WIDE("--repeat --loop") );
 	SetStopEvent( player->vlc, OnStopFade, (PTRSZVAL)player );
 
-   // only used if there is only 1 video - just show video.
+	// only used if there is only 1 video - just show video.
 	AddLink( &g.video_displays, player );
 
 	{
@@ -382,62 +420,127 @@ static PTRSZVAL CPROC SetDefaultFadeInTime( PTRSZVAL psv, arg_list args )
 	return psv;
 }
 
+static PTRSZVAL CPROC SetDefaultPrizeCount( PTRSZVAL psv, arg_list args )
+{
+	PARAM( args, S_64, index );
+	PARAM( args, S_64, count );
+	TEXTCHAR value[32];
+	struct video_sequence *seq = GetSequence( (int)index );
+	tnprintf( value, 32, "%d", (int)index );
+	seq->prize_count = SACK_GetProfileInt( "Prize Counts", value, count );
+	g.prize_total += seq->prize_count;
+	return psv;
+}
+
 static void ReadConfigFile( CTEXTSTR filename )
 {
 	PCONFIG_HANDLER pch = CreateConfigurationHandler();
 	AddConfigurationMethod( pch, WIDE("%i,%i,%i,%m"), AddVideo );
 	AddConfigurationMethod( pch, WIDE("default show time=%i"), SetDefaultShowTime );
 	AddConfigurationMethod( pch, WIDE("default fade in time=%i"), SetDefaultFadeInTime );
+	AddConfigurationMethod( pch, WIDE("default prize count %i=%i"), SetDefaultPrizeCount );
 	ProcessConfigurationFile( pch, filename, 0 );
 	DestroyConfigurationHandler( pch );
 }
 
-static void CPROC ReadPacket( PCLIENT pc, POINTER buffer, size_t size, SOCKADDR *addr )
+static void PickPrize( void )
 {
-	if( !buffer )
+	_32 rand = genrand_int32( g.rng );
+	_32 value = ( (_64)g.prize_total * (_64)rand ) / 0xFFFFFFFF;
+	struct video_sequence *seq;
+	INDEX idx;
+	lprintf( "got numbr %d %d", rand, value );
+	LIST_FORALL( g.playlists, idx, struct video_sequence *, seq )
 	{
-		buffer = NewArray( _8, 256 );
-	}
-	else
-	{
-		// packet is _32[0] = tick _32[1] = command(to be implemented, _32[2] = next chain (command 0)
-		P_32 packet = (P_32)buffer;
-		if( g.last_received != packet[0] )
+		lprintf( "is %d < %d", value, seq->prize_count );
+		if( value < seq->prize_count )
 		{
-			g.last_received = packet[0];
-         if( packet[1] == 0 )
+			g.want_next_chain = 1 + seq->sequence;
+			seq->prize_count--;
+			g.prize_total--;
+			g.target_in_start = timeGetTime();
+			lprintf( "pick prize; set next to %d", seq->sequence );
+			g.current_list[g.next_up] = seq;
+			seq->current_image = 0;
 			{
-				struct video_sequence *sequence = GetSequenceEx( packet[2], FALSE );
-				if( sequence )
-					g.next_chain = packet[2] + 1;
+				TEXTCHAR value[32];
+				tnprintf( value, 32, "%d", (int)seq->sequence );
+				SACK_WriteProfileInt( "Prize Counts", value, seq->prize_count );
 			}
-			if( packet[1] == 1 )
+			break;
+		}
+		value -= seq->prize_count;
+	}
+}
+
+static LOGICAL CPROC PressSomeKey( PTRSZVAL psv, _32 key )
+{
+	static _32 _tick, tick;
+	static int reset = 0;
+	static int reset2 = 0;
+	static int reset3 = 0;
+	static int reset4 = 0;
+	tick = timeGetTime();
+	lprintf( "got key %08x  (%d)  %d ", key, psv, tick - _tick );
+	if( !_tick || ( _tick < ( tick - 2000 ) ) )
+	{
+		lprintf( "late enough" );
+		reset4 = 0;
+		reset3 = 0;
+		reset2 = 0;
+		reset = 0;
+		g.begin_card = 0;
+		g.number_collector = 0;
+	}
+	_tick = tick;
+
+	{
+		lprintf( "continue sequence... begin new collections" );
+		if( psv < 10 )
+		{
+			// reset to new value
+			g.number_collector = ( g.number_collector * 10 ) + psv;
+			g.value_collector[g.value_collect_index++] = psv + '0';
+			g.value_collector[g.value_collect_index] = 0;
+			lprintf( "new value %d (%s)", g.number_collector, g.value_collector );
+			if( g.attract_mode )
 			{
-				// hide myself
-			}
-			if( packet[1] == 2 )
-			{
-				// reveal myself
-			}
-			if( packet[1] == 3 )
-			{
-				// position myself
+				if( g.number_collector == 13579 )
+				{
+					g.attract_mode = 0;
+					// last one is the screen to show to swipe card...
+					g.want_next_chain = g.nPlayerLists;
+					g.target_in_start = tick;
+					lprintf( "next is still this...(last)"  );
+
+					g.current_list[g.next_up] = (struct video_sequence*)GetLink( &g.playlists, g.nPlayerLists - 1 );
+				}
 
 			}
 		}
+		else if( psv == 10 )
+		{
+			lprintf( "Begin swipe..." );
+			if( !g.attract_mode )
+			{
+				g.begin_card = 1;
+				g.number_collector = 0;
+				g.value_collect_index = 0;
+				g.value_collector[g.value_collect_index] = 0;
+			}
+		}
+		else if( psv == 11 ) // '?'
+		{
+			lprintf( "end card with (%s)", g.value_collector );
+			if( !g.attract_mode )
+			{
+				PickPrize();
+			}
+			else
+				lprintf( "Still in attract?" );
+		}
 	}
-	ReadUDP( pc, buffer, 256 );
-}
-
-static void BeginNetwork( void )
-{
-	TEXTCHAR host_addr[256];
-	SACK_GetProfileString( GetProgramName(), WIDE("Interface Address"), WIDE("0.0.0.0:3020"), host_addr, sizeof( host_addr ) );
-	if( NetworkStart() )
-	{
-		g.host_socket = ServeUDP( host_addr, 3020, ReadPacket, NULL );
-		UDPEnableBroadcast( g.host_socket, TRUE );
-	}
+	return TRUE;
 }
 
 SaneWinMain(argc, argv )
@@ -449,13 +552,30 @@ SaneWinMain(argc, argv )
 	g.pii = GetImageInterface();
 	RegisterIcon( NULL );
 
-	BeginNetwork();
-
 	GetDisplaySizeEx( 0, &x, &y, &width, &height );
 	g.w = width;
 	g.h = height;
 	g.x = x;
 	g.y = y;
+
+	{
+		_32 tick = timeGetTime();
+		g.rng = init_by_array( &tick, 1 );
+	}
+
+	BindEventToKey( NULL, KEY_0, 0, PressSomeKey, (PTRSZVAL)0 );
+	BindEventToKey( NULL, KEY_1, 0, PressSomeKey, (PTRSZVAL)1 );
+	BindEventToKey( NULL, KEY_2, 0, PressSomeKey, (PTRSZVAL)2 );
+	BindEventToKey( NULL, KEY_3, 0, PressSomeKey, (PTRSZVAL)3 );
+	BindEventToKey( NULL, KEY_4, 0, PressSomeKey, (PTRSZVAL)4 );
+	BindEventToKey( NULL, KEY_5, 0, PressSomeKey, (PTRSZVAL)5 );
+	BindEventToKey( NULL, KEY_6, 0, PressSomeKey, (PTRSZVAL)6 );
+	BindEventToKey( NULL, KEY_7, 0, PressSomeKey, (PTRSZVAL)7 );
+	BindEventToKey( NULL, KEY_8, 0, PressSomeKey, (PTRSZVAL)8 );
+	BindEventToKey( NULL, KEY_9, 0, PressSomeKey, (PTRSZVAL)9 );
+	BindEventToKey( NULL, KEY_9, 0, PressSomeKey, (PTRSZVAL)9 );
+	BindEventToKey( NULL, KEY_5, KEY_MOD_SHIFT, PressSomeKey, (PTRSZVAL)10 );
+	BindEventToKey( NULL, KEY_SLASH, KEY_MOD_SHIFT, PressSomeKey, (PTRSZVAL)11 );
 
 	{
 		int state = 0;
@@ -507,6 +627,7 @@ SaneWinMain(argc, argv )
 		struct video_sequence *sequence;
 		sequence = GetSequence( 0 );
 
+
 		if( g.nTotalImages )
 		{
 			g.displays[0] = OpenDisplaySizedAt( DISPLAY_ATTRIBUTE_LAYERED|DISPLAY_ATTRIBUTE_CHILD|DISPLAY_ATTRIBUTE_NO_MOUSE|DISPLAY_ATTRIBUTE_NO_AUTO_FOCUS
@@ -529,6 +650,7 @@ SaneWinMain(argc, argv )
 
 			if( sequence->nImages )
 			{
+				g.attract_mode = 1;
 				g.next_chain = 1;
 				g.target_in_start = timeGetTime();
 			}
