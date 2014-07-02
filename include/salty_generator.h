32c1de92c2be827a93991060a08b63b20bd72408
Jim <d3x0r@users.noreply.github.com>
2014-07-02 16:26:48 -0700

    Add extra create for random context that uses sha512 instead of sha1

--- include/salty_generator.h
+++ include/salty_generator.h
@@ -9,6 +9,15 @@
 // struct random_context *entropy = CreateEntropy( void (*getsalt)( PTRSZVAL, POINTER *salt, size_t *salt_size ), PTRSZVAL psv_user );
 SRG_EXPORT struct random_context *SRG_CreateEntropy( void (*getsalt)( PTRSZVAL, POINTER *salt, size_t *salt_size ), PTRSZVAL psv_user );
 
+//
+// struct random_context *entropy = CreateEntropy2( void (*getsalt)( PTRSZVAL, POINTER *salt, size_t *salt_size ), PTRSZVAL psv_user );
+//  uses a larger salt generator...
+SRG_EXPORT struct random_context *SRG_CreateEntropy2( void (*getsalt)( PTRSZVAL, POINTER *salt, size_t *salt_size ), PTRSZVAL psv_user );
+
+// get a large number of bits of entropy from the random_context
+// buffer needs to be an integral number of 32 bit elements....
+SRG_EXPORT void SRG_GetEntropyBuffer( struct random_context *ctx, _32 *buffer, _32 bits );
+
 // get a number of bits of entropy from the
 // if get_signed is not 0, the result will be sign extended if the last bit is set
 //  (coded on little endian; tests for if ( result & ( 1 << bits - 1 ) ) then sign extend
