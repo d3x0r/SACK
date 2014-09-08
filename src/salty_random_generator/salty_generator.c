2c6a4791bd8495200dc20989e2e9e28d93049108
Panther <Panther@localhost>
2014-09-08 15:10:09 -0700

    Reformats

--- src/salty_random_generator/salty_generator.c
+++ src/salty_random_generator/salty_generator.c
@@ -7,9 +7,9 @@
 #include <salty_generator.h>
 
 
-#define MY_MASK_MASK(n,length)   (MASK_TOP_MASK(length) << ((n)&0x7) )
-#define MY_GET_MASK(v,n,mask_size)  ( ( ((MASKSET_READTYPE*)((((PTRSZVAL)v))+(n)/CHAR_BIT))[0]                                 \
- & MY_MASK_MASK(n,mask_size) )                                                                           \
+#define MY_MASK_MASK(n,length)	(MASK_TOP_MASK(length) << ((n)&0x7) )
+#define MY_GET_MASK(v,n,mask_size)  ( ( ((MASKSET_READTYPE*)((((PTRSZVAL)v))+(n)/CHAR_BIT))[0]											\
+ & MY_MASK_MASK(n,mask_size) )																									\
 	>> (((n))&0x7))
 
 
@@ -61,7 +61,7 @@ struct random_context *SRG_CreateEntropyInternal( void (*getsalt)( PTRSZVAL, POI
 	struct random_context *ctx = New( struct random_context );
 	ctx->use_version2 = version2;
 	if( ctx->use_version2 )
-      sha512_init( &ctx->sha512 );
+		sha512_init( &ctx->sha512 );
 	else
 		SHA1Reset( &ctx->sha1_ctx );
 	ctx->getsalt = getsalt;
@@ -73,12 +73,12 @@ struct random_context *SRG_CreateEntropyInternal( void (*getsalt)( PTRSZVAL, POI
 
 struct random_context *SRG_CreateEntropy( void (*getsalt)( PTRSZVAL, POINTER *salt, size_t *salt_size ), PTRSZVAL psv_user )
 {
-   return SRG_CreateEntropyInternal( getsalt, psv_user, FALSE );
+	return SRG_CreateEntropyInternal( getsalt, psv_user, FALSE );
 }
 
 struct random_context *SRG_CreateEntropy2( void (*getsalt)( PTRSZVAL, POINTER *salt, size_t *salt_size ), PTRSZVAL psv_user )
 {
-   return SRG_CreateEntropyInternal( getsalt, psv_user, TRUE );
+	return SRG_CreateEntropyInternal( getsalt, psv_user, TRUE );
 }
 
 void SRG_GetEntropyBuffer( struct random_context *ctx, _32 *buffer, _32 bits )
@@ -95,7 +95,7 @@ void SRG_GetEntropyBuffer( struct random_context *ctx, _32 *buffer, _32 bits )
 		else
 			get_bits = bits;
 
-      // only greater... if equal just grab the bits.
+		// only greater... if equal just grab the bits.
 		if( get_bits > ( ctx->bits_avail - ctx->bits_used ) )
 		{
 			if( ctx->bits_avail - ctx->bits_used )
@@ -103,27 +103,27 @@ void SRG_GetEntropyBuffer( struct random_context *ctx, _32 *buffer, _32 bits )
 				partial_bits = ctx->bits_avail - ctx->bits_used;
 				if( partial_bits > sizeof( partial_tmp ) * 8 )
 					partial_bits = sizeof( partial_tmp ) * 8;
-            if( ctx->use_version2 )
+				if( ctx->use_version2 )
 					partial_tmp = MY_GET_MASK( ctx->entropy2, ctx->bits_used, partial_bits );
-            else
+				else
 					partial_tmp = MY_GET_MASK( ctx->entropy, ctx->bits_used, partial_bits );
 			}
 			NeedBits( ctx );
+			bits -= partial_bits;
 		}
-      else
+		else
 		{
-         if( ctx->use_version2 )
+			if( ctx->use_version2 )
 				tmp = MY_GET_MASK( ctx->entropy2, ctx->bits_used, get_bits );
-         else
+			else
 				tmp = MY_GET_MASK( ctx->entropy, ctx->bits_used, get_bits );
 			ctx->bits_used += get_bits;
 			if( partial_bits )
 			{
 				tmp |= partial_tmp << get_bits;
-				get_bits += partial_bits;
 			}
 			(*buffer++) = tmp;
-         bits -= get_bits;
+			bits -= get_bits;
 		}
 	} while( bits );
 }
@@ -131,7 +131,7 @@ void SRG_GetEntropyBuffer( struct random_context *ctx, _32 *buffer, _32 bits )
 S_32 SRG_GetEntropy( struct random_context *ctx, int bits, int get_signed )
 {
 	S_32 result;
-   SRG_GetEntropyBuffer( ctx, (_32*)&result, bits );
+	SRG_GetEntropyBuffer( ctx, (_32*)&result, bits );
 	if( get_signed )
 		if( result & ( 1 << ( bits - 1 ) ) )
 		{
@@ -139,14 +139,14 @@ S_32 SRG_GetEntropy( struct random_context *ctx, int bits, int get_signed )
 			negone <<= bits;
 			return (S_32)( result | negone );
 		}
-   return result;
+	return result;
 }
 
 void SRG_ResetEntropy( struct random_context *ctx )
 {
 	if( ctx->use_version2 )
 		sha512_init( &ctx->sha512 );
-   else
+	else
 		SHA1Reset( &ctx->sha1_ctx );
 	ctx->bits_used = 0;
 	ctx->bits_avail = 0;
