da3b67669b772a347e534ac0744d3c59bedbf8fc
d3x0r <d3x0r@users.noreply.github.com>
2017-08-03 20:00:11 -0700

    Extend options to test utf8/utf16 translation routines; implement ability to generate overlong utf8

--- src/utils/unicode_ascii/unicode_ascii.c
+++ src/utils/unicode_ascii/unicode_ascii.c
@@ -1,141 +1,154 @@
 #include <stdhdrs.h>
+#include <filesys.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 
+
+struct rune_buffer {
+	TEXTRUNE runes[16384];
+	int used;
+	struct rune_buffer *next;
+};
+
 static struct local_info
 {
+	struct rune_buffer *first, *last;
+
 	char *char_buffer;
-   wchar_t *wchar_buffer;
-	int char_buffer_len;
-   int wchar_buffer_len;
+	wchar_t *wchar_buffer;
+	size_t char_buffer_len;
+	size_t wchar_buffer_len;
 } l;
 
-
-
-int read_ascii( FILE *input )
-{
-	fseek( input, 0, SEEK_END );
-	l.char_buffer_len = ftell( input );
-	l.char_buffer = (char*)malloc( l.wchar_buffer_len );
-	fseek( input, 0, SEEK_SET );
-	fread( l.char_buffer, 1, l.char_buffer_len, input );
-	return 0;
+void pushRune( TEXTRUNE rune ) {
+	if( !l.first ) {
+		l.first = l.last = New( struct rune_buffer );
+		l.first->next = NULL;
+		l.first->used = 0;
+	} else if( l.first->used == 16384 ) {
+		l.last = ( l.last->next = New( struct rune_buffer ) );
+		l.last->next = NULL;
+		l.last->used = 0;
+	}
+	l.last->runes[l.last->used++] = rune;
 }
 
-int read_unicode( FILE *input )
+
+void read_ascii( FILE *input )
 {
-	fseek( input, 0, SEEK_END );
-	l.wchar_buffer_len = ftell( input );
+	sack_fseek( input, 0, SEEK_END );
+	l.char_buffer_len = sack_ftell( input );
+	l.char_buffer = (char*)malloc( l.char_buffer_len );
+	sack_fseek( input, 0, SEEK_SET );
+	sack_fread( l.char_buffer, l.char_buffer_len, 1, input );
+	
+	const char *pos = l.char_buffer;
+	TEXTRUNE rune;
+
+	while( ( (size_t)( pos - l.char_buffer ) < l.char_buffer_len ) && ( rune = GetUtfChar( &pos ) ) )
+		pushRune( rune );
+
+}
+
+void read_unicode( FILE *input )
+{
+	sack_fseek( input, 0, SEEK_END );
+	l.wchar_buffer_len = sack_ftell( input );
 	l.wchar_buffer = (wchar_t*)malloc( l.wchar_buffer_len );
-	fseek( input, 0, SEEK_SET );
-	fread( l.wchar_buffer, 1, l.wchar_buffer_len, input );
-	return 0;
+	sack_fseek( input, 0, SEEK_SET );
+	sack_fread( l.wchar_buffer, l.wchar_buffer_len, 1, input );
+
+	const wchar_t *pos = l.wchar_buffer;
+	l.wchar_buffer_len >>= 1;
+	TEXTRUNE rune;
+	if( pos[0] == 0xFEFF ) pos++;
+	else if( pos[0] == 0xFEFF ) {
+		printf( "big endian unicode not supported." );
+		exit( 0 );
+	}
+	while( ( (size_t)( pos - l.wchar_buffer ) < l.wchar_buffer_len ) && ( rune = GetUtfCharW( &pos ) ) )
+		pushRune( rune );
 }
 
-int write_ascii( FILE *input )
+void write_ascii( FILE *input, LOGICAL overlong )
 {
-	fseek( input, 0, SEEK_SET );
-	fwrite( l.char_buffer, 1, l.char_buffer_len, input );
-	return 0;
+	sack_fseek( input, 0, SEEK_SET );
+	{
+		struct rune_buffer *outbuf = l.first;
+		int c;
+		while( outbuf ) {
+			for( c = 0; c < outbuf->used; c++ ) {
+				char buf[7];
+				int chars = ConvertToUTF8Ex( buf, outbuf->runes[c], overlong );
+				sack_fwrite( buf, 1, chars, input );
+			}
+			outbuf = outbuf->next;
+		}
+	}
 }
 
-int write_unicode( FILE *input )
+void write_unicode( FILE *input )
 {
-	fseek( input, 0, SEEK_SET );
-   fwrite( "\xFE\xFF", 1, 2, input );
-   fwrite( l.wchar_buffer, 1, l.wchar_buffer_len, input );
-   return 0;
-}
-
-void unicode_to_ascii ( void  )
-{
-	// Conversion to char* :
-	// Can just convert wchar_t* to char* using one of the 
-	// conversion functions such as: 
-	// WideCharToMultiByte()
-	// wcstombs_s()
-	// ... etc
-	int len;
-	size_t convertedChars = 0;
-	size_t  sizeInBytes;
-#if defined( _MSC_VER)
-	errno_t err;
-#else
-	int err;
-#endif
-	len = l.wchar_buffer_len;
-
-	sizeInBytes = ((len + 1) * sizeof( char ));
-   l.char_buffer_len = sizeInBytes;
-	err = 0;
-	l.char_buffer = (char*)malloc( sizeInBytes);
-#if defined( _MSC_VER )
-	err = wcstombs_s(&convertedChars, 
-                    l.char_buffer, sizeInBytes,
-						  l.wchar_buffer, sizeInBytes);
-#else
-	convertedChars = wcstombs( l.char_buffer, l.wchar_buffer, l.char_buffer_len);
-   err = ( convertedChars == -1 );
-#endif
-	if (err != 0)
-		lprintf( WIDE("wcstombs_s  failed!") );
-
-}
-
-void ascii_to_unicode( void )
-{
-	// Conversion to char* :
-	// Can just convert wchar_t* to char* using one of the 
-	// conversion functions such as: 
-	// WideCharToMultiByte()
-	// wcstombs_s()
-	// ... etc
-	int len;
-	size_t convertedChars = 0;
-#if defined( _MSC_VER)
-	errno_t err;
-#else
-	int err;
-#endif
-    len = l.char_buffer_len;
-	l.wchar_buffer_len = (len * sizeof( wchar_t ) );
-	err = 0;
-	l.wchar_buffer = (wchar_t*)malloc( l.wchar_buffer_len + 4);
-#if defined( _MSC_VER )
-	err = mbstowcs_s(&convertedChars, 
-                    l.wchar_buffer, (l.wchar_buffer_len+4)/sizeof(wchar_t),
-						  l.char_buffer, l.char_buffer_len );
-#else
-	convertedChars = mbstowcs( l.wchar_buffer, l.char_buffer, l.wchar_buffer_len);
-    err = ( convertedChars == -1 );
-#endif
-	if (err != 0)
-		lprintf( WIDE( "wcstombs_s  failed!" ));
-
+	sack_fseek( input, 0, SEEK_SET );
+	sack_fwrite( "\xFF\xFE", 1, 2, input );
+	{
+		struct rune_buffer *outbuf = l.first;
+		int c;
+		while( outbuf ) {
+			for( c = 0; c < outbuf->used; c++ ) {
+				wchar_t buf[7];
+				int chars = ConvertToUTF16( buf, outbuf->runes[c] );
+				sack_fwrite( buf, 2, chars, input );
+			}
+			outbuf = outbuf->next;
+		}
+	}
 }
 
 SaneWinMain( argc, argv )
 {
 	FILE *input;
+	FILE *output;
 	if( argc > 2 )
 	{
 		input = sack_fopen( 0, argv[2], WIDE("rb+") );
+		if( argc > 3 )
+			output = sack_fopen( 0, argv[3], "wb" );
+		else
+			output = input;
 		if( input )
 		{
-			if( StrCaseCmpEx( argv[1], WIDE( "u" ), 1 ) == 0 )
+			if( argv[1][0] == 'u' || argv[1][0] == 'U' )
 			{
 				read_ascii( input );
-				ascii_to_unicode();
-				write_unicode( input );
+				//ascii_to_unicode();
+				write_unicode( output );
+			}
+			else if( argv[1][0] == 'o' )
+			{
+				read_ascii( input );
+				//ascii_to_overlong();
+				write_ascii( output, TRUE );
+			}
+			else if( argv[1][0] == 'O' )
+			{
+				read_unicode( input );
+				//unicode_to_overlong();
+				write_ascii( output, TRUE );
 			}
 			else
 			{
-				read_unicode( input );
-				unicode_to_ascii();
-				write_ascii( input );
+				if( argv[1][0] == 'a' )
+					read_unicode( input );
+				else if( argv[1][0] == 'A' )
+					read_ascii( input );
+				//unicode_to_ascii();
+				write_ascii( output, FALSE );
 			}
-			fclose( input );
+			sack_fclose( input );
+			if( output != input )
+				sack_fclose( output );
 		}
 		else
 			printf( WIDE( "Failed to open %s" ), argv[2] );
@@ -143,7 +156,11 @@ SaneWinMain( argc, argv )
 	else
 	{
 		printf( WIDE("Usage: %s [u/a] [filename]\n"), argv[0] );
-		printf( WIDE("  u or a is unicode or ascii mode; unicode translates from ascii to unicode\n") );
+		printf( WIDE( "  u or U read utf16 write utf8\n" ) );
+		printf( WIDE( "  a read utf8 write utf16\n" ) );
+		printf( WIDE( "  A read utf8 write utf8\n" ) );
+		printf( WIDE("  o read ascii, output overlong utf8\n") );
+		printf( WIDE("  O read unicode, output overlong utf8\n") );
 		printf( WIDE("  ascii translates from unicode to ascii\n") );
 		printf( WIDE("  file will be written back in-place\n") );
 	}
