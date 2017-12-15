7b2db68f285a61de4d5c2f26b0e362167bf28f99
d3x0r <d3x0r@github.com>
2017-12-14 23:04:51 -0800

    fix getting configuration param type

--- include/configscript.h
+++ include/configscript.h
@@ -93,7 +93,7 @@ struct va_args_tag {
         (*(type *)((ap)[0]-((sizeof(type)+ARG_STACK_SIZE-1)&~(ARG_STACK_SIZE-1)))))
 #define my_va_arg_type(ap,type)     ( \
         (*(type *)((ap)[0]-(sizeof(enum configArgType)+(sizeof(type)+ARG_STACK_SIZE-1)&~(ARG_STACK_SIZE-1)))))
-#define my_va_next_arg_type(ap,type)     (*(type *)((ap)[0]))
+//#define my_va_next_arg_type(ap,type)     (*(type *)((ap)[0]))
 #define my_va_next_arg_type(ap)     ( ( *(enum configArgType *)((ap)[0]) ) )
 
 #define PARAM_COUNT( args ) (((int*)(args+1))[0])
