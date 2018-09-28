73a6ca9b1a3c186f2c928fa9f9f36a591500b689
d3x0r <d3x0r@no-reply.github.com>
2018-09-28 10:21:14 -0700

    Add fs vfs amalgamate

---
+++ amalgamate/fs_vfs/emk.bat
@@ -0,0 +1,8 @@
+:call emcc --bind --memory-init-file 0 -std=c++11 -O2 -o ./jsox-wasm.js -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -D__LINUX__  -s EXPORTED_FUNCTIONS="['_jsox_begin_parse','_jsox_parse_clear_state','_jsox_parse_add_data']" jsox.c
+
+:--pre-js or --post-js
+call emcc -O2 -o ./jsox-w.js -std=c++11 --bind -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  jsox.cc emscripten_bindings.cc --post-js jsoxParse.js
+
+call emcc -o ./jsox-w0.js -std=c++11 --bind -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  jsox.cc emscripten_bindings.cc --post-js jsoxParse.js
+
+call google-closure-compiler.cmd --language_out NO_TRANSPILE --formatting=pretty_print  --js=jsox-w.js --js_output_file=jsox-w-pretty.js
---
+++ amalgamate/fs_vfs/mk.bat
@@ -0,0 +1,45 @@
+:::
+:   
+:   ../../include/sack_vfs.h 
+:   ../../src/utils/virtual_file_system/vfs_fs.c 
+: 
+:   ../../src/deadstart/deadstart_core.c 
+:   ../../src/memlib/sharemem.c 
+:   ../../src/memlib/memory_operations.c 
+:   ../../src/typelib/typecode.c 
+:   ../../src/typelib/text.c 
+:   ../../src/typelib/sets.c
+:   ../../src/typelib/binarylist.c 
+: 
+:   ../../src/filesyslib/pathops.c
+:   
+:   ../../src/salty_random_generator/salty_generator.c 
+:   ../../src/contrib/sha3lib/sha3.c
+:   ../../src/contrib/sha2lib/sha2.c
+:   ../../src/contrib/sha1lib/sha.c
+
+mkdir c
+del jsox.h
+
+set RNG=%RNG% ../../src/salty_random_generator/salty_generator.c 
+set RNG=%RNG% ../../src/contrib/sha3lib/sha3.c
+set RNG=%RNG% ../../src/contrib/sha2lib/sha2.c
+set RNG=%RNG% ../../src/contrib/sha1lib/sha1.c
+
+
+:c:\tools\ppc.exe -c -K -once -ssio -sd -I../../include -p -oc/jsox.cc -DINCLUDE_LOGGING ../../include/jsox_parser.h ../../src/netlib/html5.websocket/json/jsox_parser.c ../../src/netlib/html5.websocket/json/jsox.h ../../src/deadstart/deadstart_core.c ../../src/memlib/sharemem.c ../../src/memlib/memory_operations.c ../../src/typelib/typecode.c ../../src/typelib/text.c ../../src/typelib/sets.c ../../src/typelib/binarylist.c 
+c:\tools\ppc.exe -c -K -once -ssio -sd -I../../include -p -oc/vfs_fs.cc -DINCLUDE_LOGGING ../../include/sack_vfs.h ../../src/filesyslib/pathops.c ../../src/utils/virtual_file_system/vfs_fs.c  ../../src/deadstart/deadstart_core.c ../../src/memlib/sharemem.c ../../src/memlib/memory_operations.c ../../src/typelib/typecode.c ../../src/typelib/text.c ../../src/typelib/sets.c ../../src/typelib/binarylist.c  %RNG% 
+copy c\vfs_fs.cc c\vfs_fs.c
+mkdir h
+cd h
+c:\tools\ppc.exe -c -K -once -ssio -sd -I../../../include -p -ovfs_fs.h ../../../include/sack_vfs.h 
+
+cd ..
+copy c\*
+copy h\vfs_fs.h
+
+gcc -g -o a tester.c vfs_fs.c
+copy tester.c tester.cc
+gcc -g -o c tester.cc vfs_fs.cc
+
+:_CRT_NONSTDC_NO_DEPRECATE;NO_OPEN_MACRO;_DEBUG;NO_OPEN_MACRO;__STATIC__;USE_SQLITE;USE_SQLITE_INTERFACE;FORCE_COLOR_MACROS;NO_OPEN_MACRO;__STATIC__;NO_FILEOP_ALIAS;_CRT_SECURE_NO_WARNINGS;NEED_SHLAPI;NEED_SHLOBJ;JSON_PARSER_MAIN_SOURCE;SQLITE_ENABLE_LOCKING_STYLE=0;MINIMAL_JSON_PARSE_ALLOCATE
