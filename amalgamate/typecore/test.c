23438dba21b37e797cbec9be214cd3b99124790b
d3x0r <d3x0r@no-reply.github.com>
2018-10-22 20:45:43 -0700

    reformat

--- amalgamate/typecore/test.c
+++ amalgamate/typecore/test.c
@@ -3,10 +3,10 @@
 int main( void ) {
 	PLIST list = NULL;
 	INDEX idx;
-   char *name;
+	char *name;
 	AddLink( &list, "asdf" );
 	LIST_FORALL( list, idx, char *, name ) {
 		printf( "list has: %d = %s\n", idx, name );
 	}
-   return 0;
+	return 0;
 }
