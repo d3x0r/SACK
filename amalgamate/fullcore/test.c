70e491c3bb57d0a253daabbd9fe35cf784cbc592
d3x0r <d3x0r@no-reply.github.com>
2018-10-24 00:39:14 -0700

    make this build; omit a few additional features

---
+++ amalgamate/fullcore/test.c
@@ -0,0 +1,12 @@
+#include "sack_ucb_full.h"
+
+int main( void ) {
+	PLIST list = NULL;
+	INDEX idx;
+	char *name;
+	AddLink( &list, "asdf" );
+	LIST_FORALL( list, idx, char *, name ) {
+		printf( "list has: %d = %s\n", idx, name );
+	}
+	return 0;
+}
