291edb896cc9e17b51c77ff22f19ed42fd7718d0
d3x0r <d3x0r@no-reply.github.com>
2018-10-23 22:15:48 -0700

    Update typelib script for micr-c-library alias

--- amalgamate/typecore/mk.bat
+++ amalgamate/typecore/mk.bat
@@ -4,6 +4,9 @@
 :   ../../src/typelib/text.c 
 :   ../../src/typelib/sets.c
 :   ../../src/typelib/binarylist.c 
+:   ../../src/typelib/familytree.c 
+:   ../../src/typelib/http.c 
+:   ../../src/typelib/url.c 
 :   ../../src/memlib/sharemem.c 
 :   ../../src/memlib/memory_operations.c 
 :   ../../src/deadstart/deadstart_core.c 
@@ -13,28 +16,34 @@
 @set SRCS=
 @set SRCS= %SRCS%   ../../src/typelib/typecode.c 
 @set SRCS= %SRCS%   ../../src/typelib/text.c 
+@set SRCS= %SRCS%   ../../src/typelib/input.c
 @set SRCS= %SRCS%   ../../src/typelib/sets.c
 @set SRCS= %SRCS%   ../../src/typelib/binarylist.c 
+@set SRCS= %SRCS%   ../../src/typelib/url.c
 @set SRCS= %SRCS%   ../../src/fractionlib/fractions.c
+:@set SRCS= %SRCS%   ../../src/vectlib/vectlib.c
+:@set SRCS= %SRCS%   ../../src/filesyslib/winfiles.c
 @set SRCS= %SRCS%   ../../src/memlib/sharemem.c 
 @set SRCS= %SRCS%   ../../src/memlib/memory_operations.c 
 @set SRCS= %SRCS%   ../../src/deadstart/deadstart_core.c 
  
 
+del sack_ucb_typelib.h
+del sack_ucb_typelib.c
 
-c:\tools\ppc.exe -c -K -once -ssio -sd -I../../include -p -osack_typelib.c -DINCLUDE_LOGGING %SRCS%
+c:\tools\ppc.exe -c -K -once -ssio -sd -I../../include -p -osack_ucb_typelib.c -DINCLUDE_LOGGING %SRCS%
 
 mkdir h
 copy config.ppc.h h\config.ppc
 cd h
 
-c:\tools\ppc.exe -c -K -once -ssio -sd -I../../../include -p -o../sack_typelib.h ../../../include/stdhdrs.h ../../../include/fractions.h
+c:\tools\ppc.exe -c -K -once -ssio -sd -I../../../include -p -o../sack_ucb_typelib.h ../../../include/stdhdrs.h ../../../include/fractions.h ../../../include/vectlib.h
 cd ..
 
-gcc -c -g -o a.o sack_typelib.c
-gcc -c -O3 -o a-opt.o sack_typelib.c
+gcc -c -g -o a.o sack_ucb_typelib.c
+gcc -c -O3 -o a-opt.o sack_ucb_typelib.c
 
-gcc -g -o a.exe sack_typelib.c test.c
-gcc -O3 -o a-opt.exe sack_typelib.c test.c
+gcc -g -o a.exe sack_ucb_typelib.c test.c
+gcc -O3 -o a-opt.exe sack_ucb_typelib.c test.c
 
 :_CRT_NONSTDC_NO_DEPRECATE;NO_OPEN_MACRO;_DEBUG;NO_OPEN_MACRO;__STATIC__;USE_SQLITE;USE_SQLITE_INTERFACE;FORCE_COLOR_MACROS;NO_OPEN_MACRO;__STATIC__;NO_FILEOP_ALIAS;_CRT_SECURE_NO_WARNINGS;NEED_SHLAPI;NEED_SHLOBJ;JSON_PARSER_MAIN_SOURCE;SQLITE_ENABLE_LOCKING_STYLE=0;MINIMAL_JSON_PARSE_ALLOCATE
