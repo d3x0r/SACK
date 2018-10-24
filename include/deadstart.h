1371d97a4c19b863441a34f46a42c17abbf6d9f7
d3x0r <d3x0r@no-reply.github.com>
2018-10-23 22:24:54 -0700

    fix missing permutation of junkinit

--- include/deadstart.h
+++ include/deadstart.h
@@ -476,11 +476,13 @@ struct rt_init // structure placed in XI/YI segment
 } __attribute__((packed));
 
 #if defined( _DEBUG ) || defined( _DEBUG_INFO )
-#if defined( __GNUC__ ) && defined( __64__)
-#define JUNKINIT(name) ,&pastejunk(name,_ctor_label), {0,0}
+#  if defined( __GNUC__ ) && defined( __64__)
+#    define JUNKINIT(name) ,&pastejunk(name,_ctor_label), {0,0}
+#  else
+#    define JUNKINIT(name) ,&pastejunk(name,_ctor_label)
+#  endif
 #else
-#define JUNKINIT(name) ,&pastejunk(name,_ctor_label)
-#endif
+#  define JUNKINIT(name) ,&pastejunk(name,_ctor_label)
 #endif
 
 #define RTINIT_STATIC static
