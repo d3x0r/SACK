4cdbf78c14dc15aa5e72c7579e4d40fa0cd0366b
d3x0r <d3x0r@no-reply.github.com>
2019-01-25 15:51:45 -0800

    Update for refined procreg api

--- src/games/router/board/methods.hpp
+++ src/games/router/board/methods.hpp
@@ -1,34 +1,34 @@
 #include "board.hpp"
 
 #define OnPeiceCreate(name)  \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPieceCreate,WIDE("board"),name WIDE("/OnCreate"),WIDE("f"),uintptr_t,(uintptr_t,PLAYER_DATA), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPieceCreate,WIDE("board/") name WIDE("/OnCreate"),WIDE("f"),"no-desc",uintptr_t,(uintptr_t,PLAYER_DATA), __LINE__)
 
 #define OnPeiceDestroy(name)  \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceDestroy,WIDE("board"),name WIDE("/OnDestroy"),WIDE("f"),void,(uintptr_t), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceDestroy,WIDE("board") name WIDE("/OnDestroy"),WIDE("f"),"no-desc",void,(uintptr_t), __LINE__)
 
 #define OnPeiceTap(name)  \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceTap,WIDE("board"),name WIDE("/OnMouseTap"),WIDE("f"),int,(uintptr_t,int32_t,int32_t), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceTap,WIDE("board") name WIDE("/OnMouseTap"),WIDE("f"),"no-desc",int,(uintptr_t,int32_t,int32_t), __LINE__)
 
 #define OnPeiceClick(name)  \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceClick,WIDE("board"),name WIDE("/OnMouseDown"),WIDE("f"),int,(uintptr_t,int32_t,int32_t), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceClick,WIDE("board") name WIDE("/OnMouseDown"),WIDE("f"),"no-desc",int,(uintptr_t,int32_t,int32_t), __LINE__)
 
 #define OnPeiceBeginDrag(name)  \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceBeginDrag,WIDE("board"),name WIDE("/OnMouseBeginDrag"),WIDE("f"),int,(uintptr_t,int32_t,int32_t), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceBeginDrag,WIDE("board") name WIDE("/OnMouseBeginDrag"),WIDE("f"),"no-desc",int,(uintptr_t,int32_t,int32_t), __LINE__)
 
 #define OnPeiceProperty( name ) \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceProperty,WIDE("board"),name WIDE("/Properties"),WIDE("f"),void,(uintptr_t,PSI_CONTROL), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceProperty,WIDE("board") name WIDE("/Properties"),WIDE("f"),"no-desc",void,(uintptr_t,PSI_CONTROL), __LINE__)
 
 #define OnPeiceBeginConnect( name ) \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceBeginConnect,WIDE("board"),name WIDE("/OnBeginConnect"),WIDE("f"),int,(uintptr_t,int32_t,int32_t,PIPEICE,uintptr_t), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceBeginConnect,WIDE("board") name WIDE("/OnBeginConnect"),WIDE("f"),"no-desc",int,(uintptr_t,int32_t,int32_t,PIPEICE,uintptr_t), __LINE__)
 
 #define OnPeiceEndConnect( name ) \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceEndConnect,WIDE("board"),name WIDE("/OnEndConnect"),WIDE("f"),int,(uintptr_t,int32_t,int32_t,PIPEICE,uintptr_t), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceEndConnect,WIDE("board") name WIDE("/OnEndConnect"),WIDE("f"),"no-desc",int,(uintptr_t,int32_t,int32_t,PIPEICE,uintptr_t), __LINE__)
 
 #define OnPeiceExtraDraw( name ) \
-   __DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceExtraDraw,WIDE("board"),name WIDE("/OnEndConnect"),WIDE("f"),void,( uintptr_t psv, Image surface, int32_t x, int32_t y, _32 w, _32 h ), __LINE__)
+   DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceExtraDraw,WIDE("board") name WIDE("/OnEndConnect"),WIDE("f"),"no-desc",void,( uintptr_t psv, Image surface, int32_t x, int32_t y, _32 w, _32 h ), __LINE__)
 
 #define OnPeiceDraw( name ) \
-	__DefineRegistryMethodP(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceDraw,WIDE("board"),name WIDE("/OnDraw"),WIDE("f"),void,( uintptr_t psv, Image surface, Image peice, int32_t x, int32_t y ), __LINE__)
+	DefineRegistryMethod2P(DEFAULT_PRELOAD_PRIORITY,WIDE("automaton"),OnPeiceDraw,WIDE("board") name WIDE("/OnDraw"),WIDE("f"),"no-desc",void,( uintptr_t psv, Image surface, Image peice, int32_t x, int32_t y ), __LINE__)
 
 //   ( uintptr_t psv, Image surface, int32_t x, int32_t y, _32 w, _32 h )
 #define BeginConnectFrom( name ) gasga
