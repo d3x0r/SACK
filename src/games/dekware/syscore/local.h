575c15c337e204981780f4ff80b47a9088a29122
d3x0r <d3x0r@no-reply.github.com>
2019-05-24 13:12:59 -0700

    Split some definitions to shared header

---
+++ src/games/dekware/syscore/local.h
@@ -0,0 +1,15 @@
+
+
+typedef TEXTCHAR* (CPROC* RegisterRoutinesProc)(void);
+typedef void (CPROC* UnloadPluginProc)(void);
+
+typedef struct plugin_tag {
+	//	HANDLE hModule;
+	RegisterRoutinesProc RegisterRoutines;
+	//UnloadPluginProc Unload;
+	// maybe track all device_tags and routine_tags registered...
+	struct plugin_tag* pNext, * pPrior;
+	char* pVersion;
+	TEXTCHAR pName[];
+} PLUGIN, * PPLUGIN;
+
