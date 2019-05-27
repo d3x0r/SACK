3ba507a670dd37bf597ce7601dcf6e7b172f0355
d3x0r <d3x0r@no-reply.github.com>
2019-05-27 15:15:30 -0700

    Initial commit

---
+++ amalgamate/wasmgui/libs/loader.c
@@ -0,0 +1,95 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <emscripten/html5.h>
+#include <emscripten.h>
+
+#define NULL ((void*)0)
+
+void initFileSystem( void ) {
+EM_ASM((
+    //FS.mkdir('/IDBFS');
+    FS.mount(IDBFS, {}, '/home/web_user');
+if(0)
+    FS.syncfs(true, function (err) {
+console.log( "Sync happened?", err );
+        assert(!err);
+    }); // sync FROM backing store
+));
+
+}
+
+#define USE_MAIN_LOOP
+
+
+static void renderLoop( void* userData ) {
+
+	extern int SACK_Vidlib_DoRenderPass( void );
+
+	//printf( "do render pass.\n " );
+	if( SACK_Vidlib_DoRenderPass() ) {
+#ifndef USE_MAIN_LOOP 
+		emscripten_request_animation_frame( renderLoop, NULL );
+#endif
+		//printf( "rechedule immediate.\n " );
+		// wants reschedule
+	}
+	else
+	{
+#ifdef USE_MAIN_LOOP
+		emscripten_pause_main_loop();
+#endif
+	}
+	
+}
+
+static void wakeMain( void ) {
+#ifndef USE_MAIN_LOOP
+  	emscripten_request_animation_frame( renderLoop, NULL );
+#else
+	emscripten_resume_main_loop();
+#endif
+}
+
+void initDisplay() {
+	EMSCRIPTEN_RESULT r;
+	void *myState = NULL;
+	{
+		extern int EditOptionsEx( void *odbc, void *parent, int wait );
+      		EditOptionsEx( NULL, NULL, 0 );
+	}
+	{
+		extern void SACK_Vidlib_SetAnimationWake( void (*)(void) );
+		SACK_Vidlib_SetAnimationWake( wakeMain );
+	}
+#ifndef USE_MAIN_LOOP
+	renderLoop( NULL );
+#else
+	emscripten_set_main_loop( renderLoop, 0, 0 );
+#endif
+
+}
+
+
+int main( void ) {
+	extern void InvokeDeadstart();
+   	initFileSystem();
+	InvokeDeadstart();
+	initDisplay();
+
+   	printf( "Hahaha\n" );
+
+	EM_ASM((
+		//FS.mkdir('/IDBFS');
+
+		FS.syncfs(true, function (err) {
+		console.log( "Sync happened?", err );
+			assert(!err);
+		}); // sync FROM backing store
+	));
+#ifndef USE_MAIN_LOOP
+	emscripten_exit_with_live_runtime();
+#endif
+}
---
+++ amalgamate/wasmgui/libs/mk.bat
@@ -0,0 +1,56 @@
+
+set CFLAGS=-D__NO_OPTIONS__ -D__NO_ODBC__ -D__WASM__ -D__LINUX__ -DMAKE_RCOORD_SINGLE -D__STATIC_GLOBALS__ ^
+   -D__MANUAL_PRELOAD__
+: -DUSE_SQLITE
+: -D__NO_MMAP__
+: -D__NO_OPTIONS__ 
+
+set CFLAGS=%CFLAGS% -I../../../src/contrib/sqlite/3.27.1-TableAlias
+set CFLAGS=%CFLAGS% -std=gnu99
+set CFLAGS=%CFLAGS%  -s ASSERTIONS=1 
+
+set LIBS=
+set LIBS=%LIBS% jpeg9.lo
+set LIBS=%LIBS% png.lo
+set LIBS=%LIBS% zlib.lo
+set LIBS=%LIBS% freetype.lo
+
+:set LIBS=%LIBS% imglib.lo 
+set LIBS=%LIBS% imglib_puregl2.lo 
+set LIBS=%LIBS% vidlib_puregl2.lo 
+
+set LIBS=%LIBS% genx.lo
+set LIBS=%LIBS% expat.lo
+set LIBS=%LIBS% psi.lo
+
+:set MORESRCS=..\..\..\src\contrib\sqlite\3.27.1-TableAlias\sqlite3.c
+
+set CFLAGS=%CFLAGS% -DSUPPORT_LOG_ALLOCATE -DDEFAULT_OUTPUT_STDERR
+set CFLAGS=%CFLAGS% -Wno-parentheses -Wno-null-dereference
+set CFLAGS=%CFLAGS%                     -Wno-address-of-packed-member
+
+
+
+call emcc -s WASM=1 -g -D_DEBUG %CFLAGS% loader.c %LIBS% %MORESRCS%  sack_ucb_full.c -o x.js
+
+set LIBS=
+set LIBS=%LIBS% jpeg9o.lo
+set LIBS=%LIBS% pngo.lo
+set LIBS=%LIBS% zlibo.lo
+set LIBS=%LIBS% freetypeo.lo
+
+:set LIBS=%LIBS% imglib.lo 
+set LIBS=%LIBS% imglib_puregl2o.lo 
+set LIBS=%LIBS% vidlib_puregl2o.lo 
+
+set LIBS=%LIBS% genxo.lo
+set LIBS=%LIBS% expato.lo
+set LIBS=%LIBS% psio.lo
+
+call emcc -s WASM=1 -O3 %CFLAGS% loader.c %LIBS% %MORESRCS% sack_ucb_full.c -o xo.js
+
+
+
+:-s EXPORT_NAME="'MyEmscriptenModule'"
+:  MODULARIZE = 0
+: https://github.com/emscripten-core/emscripten/blob/1.32.2/src/settings.js#L413
