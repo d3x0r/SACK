442c5282da63ff51ce8dc40a1d091027471464b1
d3x0r <d3x0r@no-reply.github.com>
2019-06-08 00:50:56 -0700

    Checkpoint amalgamations

--- amalgamate/fs_vfs/emk.bat
+++ amalgamate/fs_vfs/emk.bat
@@ -7,18 +7,23 @@
 
 
 set EXPORTS=
-set EXPORTS='_sack_vfs_os_load_volume'
-set EXPORTS=%EXPORTS%,'_sack_vfs_os_load_crypt_volume'
-set EXPORTS=%EXPORTS%,'_sack_vfs_os_unload_volume'
-set EXPORTS=%EXPORTS%,'_sack_vfs_os_open'
-set EXPORTS=%EXPORTS%,'_sack_vfs_system_ioctl'
-set EXPORTS=%EXPORTS%,'_sack_vfs_file_ioct'
+set EXPORTS='_initFS'
 :set EXPORTS=%EXPORTS%,
 
-:call emcc --memory-init-file 0 -s EXTRA_EXPORTED_RUNTIME_METHODS=["stringToUTF8"] -s EXPORTED_FUNCTIONS="[%EXPORTS%]" -O2 -o ./vfs-fs-w.js  -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -DUSE_STDIO=1 -DNO_SSL=1 -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  vfs_fs.c emscripten_bindings.c
-call emcc --memory-init-file 0 -s EXTRA_EXPORTED_RUNTIME_METHODS=["stringToUTF8"] -s EXPORTED_FUNCTIONS="[%EXPORTS%]" -g -o ./vfs-fs-w.js  -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -DUSE_STDIO=1 -DNO_SSL=1 -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  vfs_fs.c
-call emcc -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=["stringToUTF8"] -s EXPORTED_FUNCTIONS="[%EXPORTS%]" -g -o ./vfs-fs-wasm.js  -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -DUSE_STDIO=1 -DNO_SSL=1 -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  vfs_fs.c
 
-call emcc --memory-init-file 0 -o ./vfs-fs-w0.js   -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -DUSE_STDIO=1 -DNO_SSL=1 -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  vfs_fs.c
+@set CFLAGS=
+@set CFLAGS=%CFLAGS% -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference
+@set CFLAGS=%CFLAGS% -DUSE_STDIO=1 -DNO_SSL=1 -D__LINUX__ 
+@set CFLAGS=%CFLAGS% -s WASM=1
+
+@set CFLAGS=%CFLAGS% -D_DEBUG
+
+:-s RESERVED_FUNCTION_POINTERS=20
+
+:call emcc --memory-init-file 0 -s EXTRA_EXPORTED_RUNTIME_METHODS=["stringToUTF8"] -s EXPORTED_FUNCTIONS="[%EXPORTS%]" -O2 -o ./vfs-fs-w.js %CFLAGS%  vfs_fs.c emscripten_bindings.c
+call emcc -s EXPORTED_FUNCTIONS="[%EXPORTS%]" -g -o ./vfs-fs-w.js  %CFLAGS%  vfs_fs.c
+call emcc -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=["stringToUTF8"] -s EXPORTED_FUNCTIONS="[%EXPORTS%]" -g -o ./vfs-fs-wasm.js %CFLAGS% vfs_fs.c objStore.c
+
+:call emcc --memory-init-file 0 -o ./vfs-fs-w0.js   -Wno-address-of-packed-member -Wno-parentheses -Wno-comment -Wno-null-dereference -DUSE_STDIO=1 -DNO_SSL=1 -D__LINUX__ -s RESERVED_FUNCTION_POINTERS=20  vfs_fs.c
 
 call google-closure-compiler.cmd --language_out NO_TRANSPILE --formatting=pretty_print  --js=vfs-fs-w.js --js_output_file=vfs-fs-w-pretty.js
