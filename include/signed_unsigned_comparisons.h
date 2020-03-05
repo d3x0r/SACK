63b40b2fd2136d25a58f15fefa371f4de56603de
Jim B <d3x0r@users.noreply.github.com>
2020-03-04 21:22:15 -0800

    Update signed_unsigned_comparisons.h

--- include/signed_unsigned_comparisons.h
+++ include/signed_unsigned_comparisons.h
@@ -1,10 +1,7 @@
+// these macros test the the range of integer and unsigned
+// such that an unsigned > integer range is true if it is more than an maxint
+// and signed integer < 0 is less than any unsigned value.
 
-#if defined( _MSC_VER ) || (1)
-// huh, apparently all compiles are messed the hell up.
-#  define COMPILER_THROWS_SIGNED_UNSIGNED_MISMATCH
-#endif
-
-#ifdef COMPILER_THROWS_SIGNED_UNSIGNED_MISMATCH
 
 #  define SUS_GT(a,at,b,bt)   (((a)<0)?0:(((bt)a)>(b)))
 #  define USS_GT(a,at,b,bt)   (((b)<0)?1:((a)>((at)b)))
@@ -18,7 +15,8 @@
 #  define SUS_LTE(a,at,b,bt)  (((a)<0)?1:(((bt)a)<=(b)))
 #  define USS_LTE(a,at,b,bt)  (((b)<0)?0:((a)<=((at)b)))
 
-#else
+#if 0
+// simplified meanings of the macros
 #  define SUS_GT(a,at,b,bt)   ((a)>(b))
 #  define USS_GT(a,at,b,bt)   ((a)>(b))
 
