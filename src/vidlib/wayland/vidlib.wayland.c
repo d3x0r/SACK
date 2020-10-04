148f67713f67bff16e6c56d9bb68ec5486703376
d3x0r <d3x0r@no-reply.github.com>
2020-10-04 13:43:11 -0700

    Finish closing window more properly; begin bringing in keyboard definition stuff

--- src/vidlib/wayland/vidlib.wayland.c
+++ src/vidlib/wayland/vidlib.wayland.c
@@ -12,98 +12,9 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 
-#include <wayland-client.h>
-#include "xdg-shell-client-protocol.h"
-#include <xkbcommon/xkbcommon.h>
-#include <linux/input-event-codes.h>
-
-
+#include "local.h"
 //#define ALLOW_KDE
 
-typedef struct wvideo_tag
-{
-	struct {
-		uint32_t bShown : 1;
-		uint32_t bDestroy : 1;
-		uint32_t bFocused : 1;
-		uint32_t dirty : 1;
-		uint32_t canCommit : 1;
-		uint32_t wantDraw : 1;
-		uint32_t hidden : 1; // tracks whether the window is visible or not.
-	} flags;
-
-	int32_t x, y;
-	uint32_t w, h;
-
-	struct wvideo_tag* under;  // this window is under this reference
-	struct wvideo_tag* above;  // this window is above the one pointed here
-
-	struct wl_surface* surface;
-	struct wl_shell_surface *shell_surface;
-	struct wl_subsurface *sub_surface;
-	PCOLOR shm_data;
-	struct wl_buffer *buff;
-	int curBuffer;
-	uint32_t bufw, bufh;
-	//enum sizeDisplayValues changedEdge; // when resized, this is how to copy the existing image.
-	int freeBuffer[2];
-	struct wl_buffer * buffers[2];
-	PCOLOR  color_buffers[2];
-	size_t buffer_sizes[2];
-	Image buffer_images[2];
-	struct wl_shm_pool *pool;
-	struct wl_callback *frame_callback;  // request for WM_PAINT
-	Image pImage;
-	RedrawCallback pRedrawCallback;
-	intptr_t dwRedrawData;
-
-	MouseCallback mouse; uintptr_t psvMouse;
-	LoseFocusCallback pLoseFocus;
-	uintptr_t dwLoseFocus;
-
-	CloseCallback pWindowClose;
-	uintptr_t dwCloseData;
-
-	KeyProc pKeyProc;
-	uintptr_t dwKeyData;
-
-	HideAndRestoreCallback pHideCallback;
-	uintptr_t  dwHideData;
-
-} XPANEL, *PXPANEL;
-
-struct pointer_data {
-    struct wl_surface *surface;
-    struct wl_buffer *buffer;
-    int32_t hot_spot_x;
-    int32_t hot_spot_y;
-    struct wl_surface *target_surface;
-};
-
-struct output_data {
-	struct wl_output *wl_output;
-	int x, y;
-	int w, h;
-	int w_mm, h_mm;
-
-	int32_t scale;
-	int32_t pending_scale;
-};
-
-enum WAYLAND_INTERFACE_STRING {
-	wis_compositor,
-	wis_output,
-	wis_subcompositor,
-	wis_shell,
-	wis_seat,
-	wis_shm,
-	wis_xdg_base,
-	wis_xdg_shell,
-#ifdef ALLOW_KDE
-	wis_kde_shell,
-#endif
-	max_interface_versions
-};
 
 // these(name and version) should be merged into a struct.
 //static int supportedVersions[max_interface_versions] = {4,1,1,1,7,1,1,1};
@@ -125,61 +36,6 @@ static struct interfaceVersionInfo {
 };
 
 
-struct wayland_local_tag
-{
-	PTHREAD waylandThread;
-	PIMAGE_INTERFACE pii;
-	struct {
-		volatile uint32_t bInited : 1;
-	} flags;
-	PLIST damaged;
-	int dirty;
-	int commit;
-
-	struct wl_event_queue* queue;
-	struct wl_display* display;
-	struct wl_compositor* compositor;
-	PLIST outputSurfaces;  // contains wl_output's
-	struct wl_subcompositor* subcompositor;
-	struct wl_shm *shm;
-	volatile int registering; // valid pixel format found.
-	int canDraw; // valid pixel format found.
-	struct wl_shell *shell;
-	struct xdg_wm_base *xdg_wm_base;
-	struct xdg_shell *xdg_shell;
-	struct wl_seat *seat;
-	struct wl_pointer *pointer;
-	int32_t mouseSerial;
-	struct pointer_data pointer_data ;
-	struct wl_keyboard *keyboard;
-	struct xkb_context *xkb_context;
-	struct xkb_keymap *keymap;
-	struct xkb_state *xkb_state;
-	int versions[max_interface_versions]; // reported versions
-
-
-	int screen_width;
-	int screen_height;
-	int screen_num;
-/* these variables will be used to store the IDs of the black and white */
-/* colors of the given screen. More on this will be explained later.    */
-	unsigned long white_pixel;
-	unsigned long black_pixel;
-
-	//-------------- Global seat/input tracking
-	struct mouse {
-		int32_t x, y;
-		uint32_t b;
-	}mouse_;
-	int keyMods;
-	uint32_t  utfKeyCode;
-	xkb_keysym_t keysym;
-
-	PXPANEL hVidFocused; // keyboard events go here
-	PXPANEL hCaptured; // send all mouse events here (probalby should-no-op this)
-	PLIST pActiveList; // non-destroyed windows are here
-
-};
 struct wayland_local_tag wl;
 
 /*
@@ -473,12 +329,14 @@ static void keyboard_leave(void *data,
 		      struct wl_keyboard *wl_keyboard,
 		      uint32_t serial,
 		      struct wl_surface *surface){
-	PXPANEL r = (PXPANEL) wl_surface_get_user_data( surface );
-	if( wl.hVidFocused && wl.hVidFocused->pLoseFocus ){
-		lprintf( "on leave lose focus?");
-		wl.hVidFocused->pLoseFocus( wl.hVidFocused->dwLoseFocus, (PRENDERER)1 );
+	if( surface ) {
+		PXPANEL r = (PXPANEL) wl_surface_get_user_data( surface );
+		if( wl.hVidFocused && wl.hVidFocused->pLoseFocus ){
+			//lprintf( "on leave lose focus?");
+			wl.hVidFocused->pLoseFocus( wl.hVidFocused->dwLoseFocus, (PRENDERER)1 );
+		}
 	}
-	//r->pLoseFocus( r->dwLoseFocus, 1 );
+	wl.hVidFocused = NULL;
 }
 
 
@@ -682,11 +540,11 @@ global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
        //wl.xdg_shell = wl_registry_bind(registry, id,
        //                          &, version);
 	} else if( n == wis_xdg_base ) {
-		/*
+
       wl.xdg_wm_base = wl_registry_bind(registry, id,
                                  &xdg_wm_base_interface, version);
 		xdg_wm_base_add_listener( wl.xdg_wm_base, &xdg_wm_base_listener, NULL );
-		*/
+
 	} else if( n == wis_seat ) {
 		wl.seat = wl_registry_bind(registry, id,
 			&wl_seat_interface,  version>2?version:version);
@@ -1181,7 +1039,7 @@ LOGICAL CreateWindowStuff(PXPANEL r, PXPANEL parent )
 		r->sub_surface = wl_subcompositor_get_subsurface( wl.subcompositor, r->surface, parent->surface );
 		wl_subsurface_set_user_data(r->sub_surface, r);
 	} else {
-		if(wl.shell) {
+		if(wl.shell && !wl.xdg_wm_base) {
 			r->shell_surface = wl_shell_get_shell_surface(wl.shell, r->surface);
 			wl_shell_surface_add_listener( r->shell_surface, &shell_surface_listener, r );
 			wl_shell_surface_set_toplevel(r->shell_surface);
@@ -1191,9 +1049,8 @@ LOGICAL CreateWindowStuff(PXPANEL r, PXPANEL parent )
 		if(wl.xdg_wm_base) {
 			r->shell_surface = (struct wl_shell_surface*)xdg_wm_base_get_xdg_surface( wl.xdg_wm_base, r->surface );
 			xdg_surface_add_listener( (struct xdg_surface*)r->shell_surface, &xdg_surface_listener, r );
-			lprintf( "HERE?" );
-			struct xdg_toplevel * xdg_toplevel = xdg_surface_get_toplevel( (struct xdg_surface*)r->shell_surface );
-			//xdg_toplevel_set_title(  xdg_toplevel, "I DOn't want a title");
+			r->xdg_toplevel = xdg_surface_get_toplevel( (struct xdg_surface*)r->shell_surface );
+			//xdg_toplevel_set_title(  r->xdg_toplevel, "I DOn't want a title");
 		   xdg_surface_set_user_data((struct xdg_surface*)r->shell_surface, r);
 			// must commit to get a config
 			wl_surface_commit( r->surface );
@@ -1232,6 +1089,8 @@ static void sack_wayland_Redraw( PRENDERER renderer ) {
 }
 
 static void sack_wayland_SetApplicationTitle( char const *title ){
+
+	//xdg_toplevel_set_title(  r->xdg_toplevel, "I DOn't want a title");
 
 }
 
@@ -1296,8 +1155,15 @@ static void sack_wayland_CloseDisplay( PRENDERER renderer ) {
 	r->flags.bDestroy = 1;
 	if( r->sub_surface )
 		wl_subsurface_destroy( r->sub_surface );
-	if( r->shell_surface )
-		wl_shell_surface_destroy( r->shell_surface );
+	if( r->shell_surface ) {
+		if( wl.xdg_wm_base ) {
+		if( r->xdg_toplevel )
+				xdg_toplevel_destroy( r->xdg_toplevel );
+			xdg_surface_destroy( (struct xdg_surface*)r->shell_surface );
+		}else
+			wl_shell_surface_destroy( r->shell_surface );
+	}
+	lprintf( "Destroy surface:%p %p", r, r->surface );
 	wl_surface_destroy( r->surface );
 	DeleteLink( &wl.pActiveList, r );
 }
@@ -1448,6 +1314,8 @@ static  int sack_wayland_IsTouchDisplay ( void ){
 
 
 static void sack_wayland_SetRendererTitle( PRENDERER render, char const* title ){
+	struct wvideo_tag *r = (struct wvideo_tag*)render;
+	xdg_toplevel_set_title(  r->xdg_toplevel, title );
 
 }
 
@@ -1553,7 +1421,11 @@ static int sack_wayland_UnbindKey( PRENDERER pRenderer, uint32_t scancode, uint3
 	PXPANEL r = (PXPANEL)renderer;
 	lprintf( "Issue Mouse Move %d", wl.mouseSerial );
  	wl.mouse_.b &= ~MK_LBUTTON;
-	 wl_shell_surface_move( r->shell_surface, wl.seat, wl.mouseSerial );
+
+	if( wl.xdg_wm_base )
+		xdg_toplevel_move( r->xdg_toplevel, wl.seat, wl.mouseSerial );
+	else
+		wl_shell_surface_move( r->shell_surface, wl.seat, wl.mouseSerial );
  }
  //static void sack_wayland_EndMoveDisplay(  PRENDERER pRenderer )
 
@@ -1561,7 +1433,10 @@ static int sack_wayland_UnbindKey( PRENDERER pRenderer, uint32_t scancode, uint3
 	PXPANEL r = (PXPANEL)renderer;
  	wl.mouse_.b &= ~MK_LBUTTON;
 	lprintf( "Issue Mouse SizeBgin %d", wl.mouseSerial );
-	 wl_shell_surface_resize( r->shell_surface, wl.seat, wl.mouseSerial, mode );
+	if( wl.xdg_wm_base )
+	 	xdg_toplevel_resize( r->xdg_toplevel, wl.seat, wl.mouseSerial, mode );
+	else
+		wl_shell_surface_resize( r->shell_surface, wl.seat, wl.mouseSerial, mode );
  }
  //static void sack_wayland_EndSizeDisplay(  PRENDERER pRenderer )
 
