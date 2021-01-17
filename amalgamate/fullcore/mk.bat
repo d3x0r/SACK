3b9e9b5aa85aa683f197f28001932252d56243f5
d3x0r <d3x0r@no-reply.github.com>
2021-01-16 16:35:50 -0800

    Update more advanced tests to build with sqlite

--- amalgamate/fullcore/mk.bat
+++ amalgamate/fullcore/mk.bat
@@ -12,8 +12,10 @@
 
 call mksrc.bat
 
-gcc -c -g -o a.o sack_ucb_full.cc
-gcc -c -O3 -o a-opt.o sack_ucb_full.cc
+@set SQLITE_OPTS=-I../../src/contrib/sqlite/3.33.0-TableAlias -DSQLITE_ENABLE_COLUMN_METADATA
+
+gcc %SQLITE_OPTS% -c -g -o a.o sack_ucb_full.cc
+gcc %SQLITE_OPTS% -c -O3 -o a-opt.o sack_ucb_full.cc
 
 @set LIBS=
 @set LIBS=%LIBS% -lwinmm 
@@ -26,13 +28,15 @@ gcc -c -O3 -o a-opt.o sack_ucb_full.cc
 @set LIBS=%LIBS% -lcrypt32
 @set LIBS=%LIBS% -lole32
 
-gcc -g -o a.exe sack_ucb_full.cc test.cc %LIBS%
-gcc -O3 -o a-opt.exe sack_ucb_full.cc test.cc %LIBS%
+@set LIBS=%LIBS% ../../src/contrib/sqlite/3.33.0-TableAlias/sqlite3.c
 
-gcc -g -o a.exe a.o test.c %LIBS%
-gcc -O3 -o a-opt.exe a-opt.o test.c %LIBS%
+gcc %SQLITE_OPTS% -g -o a.exe sack_ucb_full.cc test.cc %LIBS%
+gcc %SQLITE_OPTS% -O3 -o a-opt.exe sack_ucb_full.cc test.cc %LIBS%
 
-gcc -O3 -o a-test2.exe a-opt.o test2.c %LIBS%
+gcc %SQLITE_OPTS% -g -o a.exe a.o test.c %LIBS%
+gcc %SQLITE_OPTS% -O3 -o a-opt.exe a-opt.o test.c %LIBS%
+
+gcc %SQLITE_OPTS% -O3 -o a-test2.exe a-opt.o test2.c %LIBS%
 
 :_CRT_NONSTDC_NO_DEPRECATE;NO_OPEN_MACRO;_DEBUG;NO_OPEN_MACRO;__STATIC__;USE_SQLITE;USE_SQLITE_INTERFACE;FORCE_COLOR_MACROS;NO_OPEN_MACRO;__STATIC__;NO_FILEOP_ALIAS;_CRT_SECURE_NO_WARNINGS;NEED_SHLAPI;NEED_SHLOBJ;JSON_PARSER_MAIN_SOURCE;SQLITE_ENABLE_LOCKING_STYLE=0;MINIMAL_JSON_PARSE_ALLOCATE
 
