9ac08e653482c26fb38e00755e809018be5cb65a
d3x0r <d3x0r@no-reply.github.com>
2023-06-22 09:48:08 -0700

    fixed end of line trimming

--- src/utils/service/service_test.c
+++ src/utils/service/service_test.c
@@ -22,8 +22,9 @@ static void logOutput( uintptr_t psv, PTASK_INFO task, CTEXTSTR buffer, size_t s
 	if( buffer[size +less]=='\n' )
 		less--; else less++;
 	if( less<0 && buffer[size +less]=='\r' )
-		less--; else less++;
-	lprintf( "%.*s", size+less, buffer );
+		less--;// else less++;
+	
+	lprintf( "%.*s", size+less+1, buffer );
 }
 
 static void runTask( void ) {
