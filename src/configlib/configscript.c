446b81355dd40b73a41bbdd4ae2fe7efbc2337d4
d3x0r <d3x0r@no-reply.github.com>
2023-07-03 15:47:54 -0700

    Protect against reading an empty file and testing content

--- src/configlib/configscript.c
+++ src/configlib/configscript.c
@@ -2164,7 +2164,8 @@ static void TestUnicode( PCONFIG_HANDLER pch )
 		size_t char_check;
 		int ascii_unicode = 1;
 		len_read = sack_fread( charbuf, 1, 64, pch->file );
-		if( ( ((uint16_t*)charbuf)[0] == 0xFEFF )
+		if( len_read >= 2 &&
+			( ((uint16_t*)charbuf)[0] == 0xFEFF )
 			|| ( ((uint16_t*)charbuf)[0] == 0xFFFE )
 			|| ( ((uint16_t*)charbuf)[0] == 0xFDEF ) )
 		{
