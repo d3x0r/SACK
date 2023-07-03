d8d35c9c5585f95d6be741d0aca87476f7777c22
d3x0r <d3x0r@no-reply.github.com>
2023-07-03 15:58:06 -0700

    Fix test

--- src/configlib/configscript.c
+++ src/configlib/configscript.c
@@ -2165,9 +2165,9 @@ static void TestUnicode( PCONFIG_HANDLER pch )
 		int ascii_unicode = 1;
 		len_read = sack_fread( charbuf, 1, 64, pch->file );
 		if( len_read >= 2 &&
-			( ((uint16_t*)charbuf)[0] == 0xFEFF )
-			|| ( ((uint16_t*)charbuf)[0] == 0xFFFE )
-			|| ( ((uint16_t*)charbuf)[0] == 0xFDEF ) )
+			&& ( ( ((uint16_t*)charbuf)[0] == 0xFEFF )
+		      || ( ((uint16_t*)charbuf)[0] == 0xFFFE )
+		      || ( ((uint16_t*)charbuf)[0] == 0xFDEF ) ) )
 		{
 			return_pos = 2;
 			pch->flags.bUnicode = 1;
