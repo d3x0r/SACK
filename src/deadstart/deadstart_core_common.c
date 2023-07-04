7a5cfc9fafe6005c30b7838ec656323f5a0dd629
d3x0r <d3x0r@no-reply.github.com>
2023-07-03 23:51:41 -0700

    Add namespace wrapper

--- src/deadstart/deadstart_core_common.c
+++ src/deadstart/deadstart_core_common.c
@@ -5,7 +5,11 @@
 struct deadstart_local_data_ deadstart_local_data;
 
 #ifndef __STATIC_GLOBALS__
+SACK_DEADSTART_NAMESPACE
+
 EXPORT_METHOD struct deadstart_local_data_* GetDeadstartSharedGlobal( void ){
 	return &deadstart_local_data;
 }
+
+SACK_DEADSTART_NAMESPACE_END
 #endif
