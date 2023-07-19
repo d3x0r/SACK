e0ea8e8e54b1682b9b8a8a2184974d9e5e6c2e06
d3x0r <d3x0r@no-reply.github.com>
2023-07-19 04:47:11 -0700

    Added some debugging; wayland blew up with a bad buffer attach; but there's only one buffer, why do I have to attach it again with damage?

---
+++ src/vidlib/wayland/reference_client/README.md
@@ -0,0 +1,7 @@
+
+
+client.c is from https://wayland-book.com/xdg-shell-basics/example-code.html .  Green/red drawing
+and keyboard_protocol and pointer_protocol modules were added.
+
+connect, registry and shell are from https://jan.newmarch.name/Wayland/ProgrammingClient/
+This didn't have the surface update...
---
+++ src/vidlib/wayland/reference_client/keyboard_protocol.c
@@ -0,0 +1,71 @@
+
+#include <wayland-client.h>
+#include "xdg-shell-client-protocol.h"
+
+extern void drawRed( void );
+extern void drawGreen( void );
+
+
+static void keyboard_keymap(void *data,
+		       struct wl_keyboard *wl_keyboard,
+		       uint32_t format,
+		       int32_t fd,
+		       uint32_t size)
+{
+}
+
+static void keyboard_enter(void *data,
+		      struct wl_keyboard *wl_keyboard,
+		      uint32_t serial,
+		      struct wl_surface *surface,
+		      struct wl_array *keys)
+{
+	drawRed();
+	//wl.hVidFocused = data;
+}
+
+static void keyboard_leave(void *data,
+		      struct wl_keyboard *wl_keyboard,
+		      uint32_t serial,
+		      struct wl_surface *surface){
+	drawGreen();
+	//wl.hVidFocused = NULL;
+}
+
+
+
+static void keyboard_key(void *data,
+		    struct wl_keyboard *wl_keyboard,
+		    uint32_t serial,
+		    uint32_t time,
+		    uint32_t key,
+		    uint32_t state)
+{
+}
+
+static void keyboard_modifiers( void* data,
+	struct wl_keyboard* wl_keyboard,
+	uint32_t serial,
+	uint32_t mods_depressed,
+	uint32_t mods_latched,
+	uint32_t mods_locked,
+	uint32_t group ) {
+
+}
+
+static void keyboard_repeat_info(void *data,
+			    struct wl_keyboard *wl_keyboard,
+			    int32_t rate,
+			    int32_t delay){
+				 }
+
+
+const struct wl_keyboard_listener keyboard_listener = {
+	.keymap = keyboard_keymap,
+	.enter = keyboard_enter,
+	.leave = keyboard_leave,
+	.key = keyboard_key,
+	.modifiers = keyboard_modifiers,
+	.repeat_info = keyboard_repeat_info,
+};
+
