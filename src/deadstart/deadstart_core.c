f2171844292b17c620754e5cb0d1b2eeb4e18a2b
d3x0r <d3x0r@no-reply.github.com>
2024-08-29 02:39:03 -0700

    Fix format for DWORD parameter even though %d is also valid

--- src/deadstart/deadstart_core.c
+++ src/deadstart/deadstart_core.c
@@ -245,7 +245,7 @@ static BOOL WINAPI CtrlC( DWORD dwCtrlType )
 {
 	if( ignoreBreak & ( 1 << dwCtrlType ) ) return TRUE;
 #ifdef DEBUG_ATEXIT
-	fprintf( stderr, "Received ctrlC Event %08x %d\n", ignoreBreak, dwCtrlType );
+	fprintf( stderr, "Received ctrlC Event %08x %ld\n", ignoreBreak, dwCtrlType );
 #endif	
 	switch( dwCtrlType )
 	{
