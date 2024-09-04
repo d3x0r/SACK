a68ed5c10ad4e39ba110ceb67085b20018d80d0b
d3x0r <d3x0r@no-reply.github.com>
2024-09-04 14:10:45 -0700

    ipof to pull both ipv4 and 6?

--- src/netlib/scan/ipof.c
+++ src/netlib/scan/ipof.c
@@ -18,34 +18,36 @@ SaneWinMain( argc, argv )
 		return 1;
 	}
 	if( NetworkWait( NULL, 1, 0 ) ) {
-		SOCKADDR* sa = CreateRemote( argv[1], 5555 );
-		uint32_t IP;
-		if( sa->sa_family == AF_INET ) {
-			GetAddressParts( sa, &IP, NULL );
+		for( let i = 0; i < 2; i++ ) {
+			SOCKADDR* sa = CreateRemoteV2( argv[1], 5555, i?NETWORK_ADDRESS_FLAG_PREFER_V6:NETWORK_ADDRESS_FLAG_PREFER_V4 );
+			uint32_t IP;
+			if( sa->sa_family == AF_INET ) {
+				GetAddressParts( sa, &IP, NULL );
 
-			printf( "%"_32f ".%"_32f ".%"_32f ".%"_32f ""
-			      , ( IP & 0xFF )
-			      , ( IP & 0xFF00 ) >> 8
-			      , ( IP & 0xFF0000 ) >> 16
-			      , ( IP & 0xFF000000 ) >> 24
-			);
-		}
-		if( sa->sa_family == AF_INET6 ) {
-			//uint32_t IP[4];
+				printf( "%"_32f ".%"_32f ".%"_32f ".%"_32f ""
+				      , ( IP & 0xFF )
+				      , ( IP & 0xFF00 ) >> 8
+				      , ( IP & 0xFF0000 ) >> 16
+				      , ( IP & 0xFF000000 ) >> 24
+				);
+			}
+			if( sa->sa_family == AF_INET6 ) {
+				//uint32_t IP[4];
 			//GetAddressParts( sa, IP, NULL );
-			printf( "(%s) %03d %04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x\n"
-			      , ( ((uintptr_t*)sa)[-1] & 0xFFFF0000 )?( ((char**)sa)[-1] ) : "no name"
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+2))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+8))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+10))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+12))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+14))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+16))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+18))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+20))))
-			      , ntohs(*(((unsigned short *)((unsigned char*)sa+22))))
-			      );
-			//printf( "%
+				printf( "(%s) %03d %04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x\n"
+				      , ( ((uintptr_t*)sa)[-1] & 0xFFFF0000 )?( ((char**)sa)[-1] ) : "no name"
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+2))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+8))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+10))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+12))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+14))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+16))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+18))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+20))))
+				      , ntohs(*(((unsigned short *)((unsigned char*)sa+22))))
+				      );
+				//printf( "%
+			}
 		}
 	}
 	else
