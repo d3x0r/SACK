f26392bb9ecd6df23e7242350948232691a8c714
d3x0r <d3x0r@no-reply.github.com>
2025-02-10 05:08:11 -0500

    Protect against closing a 0 space; assume it is closed

--- src/memlib/sharemem.c
+++ src/memlib/sharemem.c
@@ -943,13 +943,14 @@ PSPACE FindSpace( POINTER pMem )
 {
 	PSPACEPOOL psp;
 	INDEX idx;
-	for( psp = g.pSpacePool;psp; psp = psp->next) 
-		for( idx = 0; idx < MAX_PER_BLOCK; idx++ ) {
-			//if( g.bLogAllocate)
-			//	lprintf( "Finding space %p %p", pMem, psp->spaces[idx].pMem);
-			if( psp->spaces[idx].pMem == pMem )
-				return psp->spaces + idx;
-		}
+	if( pMem ) // if 0; the block of spaces may be empty and matches 0.
+		for( psp = g.pSpacePool;psp; psp = psp->next) 
+			for( idx = 0; idx < MAX_PER_BLOCK; idx++ ) {
+				//if( g.bLogAllocate)
+				//	lprintf( "Finding space %p %p", pMem, psp->spaces[idx].pMem);
+				if( psp->spaces[idx].pMem == pMem )
+					return psp->spaces + idx;
+			}
 	//if( g.bLogAllocate)
 	//	lprintf( "Failed to find space %p", pMem );
 	return NULL;
@@ -989,14 +990,16 @@ static void DoCloseSpace( PSPACE ps, int bFinal )
 
  void  CloseSpaceEx ( POINTER pMem, int bFinal )
 {
-	DoCloseSpace( FindSpace( pMem ), bFinal );
+	if( pMem )
+		DoCloseSpace( FindSpace( pMem ), bFinal );
 }
 
 //------------------------------------------------------------------------------------------------------
 
  void  CloseSpace ( POINTER pMem )
 {
-	DoCloseSpace( FindSpace( pMem ), TRUE );
+	if( pMem )
+		DoCloseSpace( FindSpace( pMem ), TRUE );
 }
 //------------------------------------------------------------------------------------------------------
 
