ad834eb2566858d05a7787522bdaf16e3e6ad0df
d3x0r <d3x0r@no-reply.github.com>
2025-06-27 21:39:00 -0700

    Load multiple libraries on commandline

--- src/tests/testDllLoad.c
+++ src/tests/testDllLoad.c
@@ -2,8 +2,11 @@
 
 int main( int argc, char**argv ) {
 	if( argc > 1 ) {
-		int status = LoadLibrary( argv[1] );
-		printf( "Status is:%d", status );
+		int i;
+		for( i = 1; i < argc; i++ ) {
+			POINTER status = LoadFunction( argv[ i ], NULL );
+			printf( "%s Status is:%p", argv[i], status );
+		}
 	}else {
       printf( "Specify library name to load... %s <library>", argv[0] );
 	}
