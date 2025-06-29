cb961463966e085fba88464ce04a90780b7c5bc9
d3x0r <d3x0r@no-reply.github.com>
2025-06-28 17:07:51 -0700

    Extend test to load a name too (trigger delayload)

--- src/tests/testDllLoad.c
+++ src/tests/testDllLoad.c
@@ -1,14 +1,25 @@
-#include <stdhdrs.h>
-
+#ifdef WIN32
+#include <windows.h>
+#include <stdio.h>
 int main( int argc, char**argv ) {
 	if( argc > 1 ) {
 		int i;
-		for( i = 1; i < argc; i++ ) {
-			POINTER status = LoadFunction( argv[ i ], NULL );
-			printf( "%s Status is:%p", argv[i], status );
+		for( i = 1; i < (argc-1); i+=2 ) {
+			HMODULE status = LoadLibrary( argv[ i ] );
+			printf( "%s Status is:%p %d", argv[i], status, GetLastError() );
+			FARPROC x = GetProcAddress( status, argv[i+1] );
+			printf( "entry found: %p", x );
 		}
 	}else {
-      printf( "Specify library name to load... %s <library>", argv[0] );
+      printf( "Specify library name and function to load... %s <library> <function>", argv[0] );
 	}
    return 0;
 }
+
+
+
+#else
+int main( void ) {
+	return 0;
+}
+#endif
