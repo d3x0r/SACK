4d2ef1f4046089855142b427d731273c599e3e61
d3x0r <d3x0r@no-reply.github.com>
2025-07-07 21:35:26 -0700

    Added linux version of signal_event

---
+++ src/utils/service/signal_event_linux.c
@@ -0,0 +1,18 @@
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main( int argc, char **argv ) {
+    if( argc < 2 ) {
+        printf( "Usage: %s <Event name>\n", argv[0] );
+        return 0;
+    }
+	int dir = opendir( "/tmp" );
+	int hPipe = openat( dir, argv[1], 0666 );
+	if( hPipe >= 0 ) {
+		write( hPipe, "X", 1 );
+		printf( "Event signaled." );
+	} else
+		printf( "Failed to set event? %d\n", errno );
+	return 0;
+}
