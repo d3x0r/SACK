f6b2d9efa33f2b769f761686e8599ed82d76d4a8
d3x0r <d3x0r@no-reply.github.com>
2025-07-08 01:13:30 -0400

    Fix building signal_event on linux

--- src/utils/service/signal_event_linux.c
+++ src/utils/service/signal_event_linux.c
@@ -1,13 +1,19 @@
-
+#define _POSIX_C_SOURCE  200809L
+#define _POSIX_SOURCE
 #include <unistd.h>
+#include <errno.h>
+
+#include <dirent.h>
 #include <stdio.h>
+#include <fcntl.h>
 
 int main( int argc, char **argv ) {
     if( argc < 2 ) {
         printf( "Usage: %s <Event name>\n", argv[0] );
         return 0;
     }
-	int dir = opendir( "/tmp" );
+	int dir = open("/tmp", O_RDONLY | O_DIRECTORY);
+
 	int hPipe = openat( dir, argv[1], 0666 );
 	if( hPipe >= 0 ) {
 		write( hPipe, "X", 1 );
