db518d322435c3d85c994a8b34f6105e6af4dd6c
d3x0r <d3x0r@no-reply.github.com>
2025-12-07 21:33:53 -0800

    Update library interface

--- makefiles/prog/ppc/merge.portable/panthers_c_preprocessor.h
+++ makefiles/prog/ppc/merge.portable/panthers_c_preprocessor.h
@@ -1,18 +1,42 @@
+#ifndef PANHTHERS_C_PREPROCESSOR_INTERFACE_DEFINED
+#	define PANHTHERS_C_PREPROCESSOR_INTERFACE_DEFINED
 
 #ifndef PUBLIC 
 #  error Have to include SACK runtime headers first...
 #endif
 
 #ifdef CPP_MAIN_SOURCE
-#  define INTERFACE_METHOD PUBLIC_METHOD
+#  define INTERFACE_METHOD EXPORT_METHOD
 #else
-#  define INTERFACE_METHOD REFERENCE_METHOD
+#  define INTERFACE_METHOD IMPORT_METHOD
 #endif
 
 
 #ifdef __cplusplus
-extern "C" {
+namespace d3x0r {
+namespace ppc {
 #endif
+
+enum LocationType {
+	LOCATION_TYPE_UNIFORM = 0,
+	LOCATION_TYPE_IN,
+	LOCATION_TYPE_OUT
+};
+
+typedef struct location_binding_tag {
+	// type of this location
+	enum LocationType type;
+	// the binding/location number assigned
+	int number;
+	// type of this variable
+	char *varType;
+	// name of this variable
+	char name[ 64 ];
+	// the file this binding/ location was defined in
+	char const *file;
+	// a unique identifier passed with the file name to process
+	uintptr_t shaderInfo;
+} *PLOCATION_BINDING;
 
 // this can be used to set some internal options that I'm too lazy
 // to expose a proper API to set.
@@ -22,9 +46,10 @@ INTERFACE_METHOD int processArguments( int argc, char **argv );
 // The state after processing includes all files that were included,
 // symbols that were defined, and counters, so another file can
 // be processed in sequence with the original.
-INTERFACE_METHOD void ProcessFile( char *file, PVARTEXT pvtOutput );
+INTERFACE_METHOD void
+ProcessFile( char *file, PVARTEXT pvtOutput, uintptr_t processInfo );
 
-// Resets counters, and defined symbols to be current information and 
+// Resets counters, and defined symbols to be current information and
 // start to process a new series of files.
 INTERFACE_METHOD void ResetProcessor( void );
 
@@ -32,8 +57,24 @@ INTERFACE_METHOD void ResetProcessor( void );
 // the preprocessor should not be used after calling this.
 INTERFACE_METHOD void CloseProcessor( void );
 
+//--------------- custom interface -----------------
+
+INTERFACE_METHOD PLIST GetBindings( void );
+INTERFACE_METHOD PLIST GetLocations( void );
+
+INTERFACE_METHOD int GetFirstBinding( void );
+INTERFACE_METHOD int GetFirstLocation( void );
+
+INTERFACE_METHOD void
+DefineExternalDefine( char *name,
+                      char *args,
+                      void ( *cb )( uintptr_t psvShaderInfo, PLIST pArgs, PTEXT *pOutput ) );
+
 #ifdef __cplusplus
+	}
 }
 #endif
 
 #undef INTERFACE_METHOD
+
+#endif
