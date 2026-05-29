7988db8d0ef07bd7a9134561fdc54ec246112ed8
d3x0r <d3x0r@no-reply.github.com>
2026-05-29 13:32:13 -0700

    Sync libssh2 - add missing sources

---
+++ src/contrib/libssh2/scripts/spellcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Copyright (C) The libssh2 project and its contributors.
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -eu
+
+cd "$(dirname "$0")"/..
+
+git ls-files -z | xargs -0 -r \
+codespell \
+  --skip 'docs/AUTHORS' \
+  --ignore-words 'scripts/codespell-ignore.words' \
+  --
