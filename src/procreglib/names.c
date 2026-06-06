dc025d6276e6d6429ddcf35455d834d69ced2a26
d3x0r <d3x0r@no-reply.github.com>
2026-06-05 22:46:39 -0700

    Fix formtting, and extra =

--- src/procreglib/names.c
+++ src/procreglib/names.c
@@ -2113,18 +2113,18 @@ static uintptr_t CPROC TestOption( uintptr_t psv, arg_list args )
 	PARAM( args, CTEXTSTR, key );
 	PARAM( args, CTEXTSTR, value );
 	TEXTCHAR buf[256];
-	if( key[0] === '?' ) {
+	if( key[0] == '?' ) {
 #ifdef _WIN32
 		if( StrCaseCmp( key+1, "windows") == 0 ) {
-         return psv;
+			return psv;
 		}
 #endif
 #ifdef __LINUX__
 		if( StrCaseCmp( key+1, "linux") == 0 ) {
-         return psv;
+			return psv;
 		}
 		if( StrCaseCmp( key+1, "posix") == 0 ) {
-         return psv;
+			return psv;
 		}
 #endif
 		l.flags.bFindEndif++;
